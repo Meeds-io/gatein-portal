d623046e4a30558b5f698e6e5326c5fc99d11e7b
Nguyen The Tuyen <tuyennt@exoplatform.com>
2015-08-13 12:15:43 +0700

    EXOGTN-2070 [OAuth] Not good navigation after Link social account

--- webui/portal/src/main/java/org/exoplatform/portal/account/UIAccountSetting.java
+++ webui/portal/src/main/java/org/exoplatform/portal/account/UIAccountSetting.java
@@ -25,23 +25,59 @@ package org.exoplatform.portal.account;
  *          tungcnw@gmail.com
  */
 
+import org.exoplatform.portal.application.PortalRequestContext;
+import org.exoplatform.portal.webui.util.Util;
 import org.exoplatform.portal.webui.workspace.UIMaskWorkspace;
+import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
+import org.exoplatform.webui.core.UIComponent;
 import org.exoplatform.webui.core.UIContainer;
 
 @ComponentConfig(template = "system:/groovy/portal/webui/portal/UIAccountSettingForm.gtmpl", events = { @EventConfig(listeners = UIMaskWorkspace.CloseActionListener.class) })
 public class UIAccountSetting extends UIContainer {
+    public static final String PARAM_ACTIVE_CHILD_ID = "accountSettingActiveTab";
 
     public static final String[] ACTIONS = { "Close" };
+
+    private String activeChildId = null;
 
     public String[] getActions() {
         return ACTIONS;
     }
 
     public UIAccountSetting() throws Exception {
-        addChild(UIAccountProfiles.class, null, null);
+        addChild(UIAccountProfiles.class, null, null).setRendered(false);
         addChild(UIAccountChangePass.class, null, null).setRendered(false);
         addChild(UIAccountSocial.class, null, null).setRendered(false);
+        setActiveChildId(getChild(0).getId());
+    }
+
+    public String getActiveChildId() {
+        return activeChildId;
+    }
+
+    public void setActiveChildId(String activeChildId) {
+        UIComponent child = this.getChildById(activeChildId);
+        if (child != null) {
+            if (this.activeChildId != null && !this.activeChildId.isEmpty()) {
+                UIComponent ui = getChildById(activeChildId);
+                if (ui != null) {
+                    ui.setRendered(false);
+                }
+            }
+            child.setRendered(true);
+            this.activeChildId = activeChildId;
+        }
+    }
+
+    @Override
+    public void processRender(WebuiRequestContext context) throws Exception {
+        PortalRequestContext prContext = Util.getPortalRequestContext();
+        String childId = prContext.getRequestParameter(PARAM_ACTIVE_CHILD_ID);
+        if (childId != null && !childId.isEmpty()) {
+            setActiveChildId(childId);
+        }
+        super.processRender(context);
     }
 }
