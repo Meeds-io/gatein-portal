Meeds-io/gatein-portal

commit ffaae9e82cde2a8354ced7939c9ff49e9e94033d

EXOGTN-2320 Optimize MOP cache for cluster (#149)

component/common/src/main/java/org/exoplatform/commons/cache/future/FutureCache.java
@@ -131,7 +131,7 @@ public abstract class FutureCache<K, V, C> {
                         retrieval.current = Thread.currentThread();
                         retrieval.future.run();
                     } catch (Exception e) {
-                        log.error("Retrieval of resource " + key + " threw an exception", e);
+                        throw new RuntimeException("Retrieval of resource " + key + " threw an exception", e);
                     } finally {
                         retrieval.current = null;
                     }
@@ -145,7 +145,7 @@ public abstract class FutureCache<K, V, C> {
                     try {
                         value = retrieval.future.get();
                     } catch (ExecutionException e) {
-                        log.error("Computing of resource " + key + " threw an exception", e.getCause());
+                      throw new RuntimeException("Computing of resource " + key + " threw an exception", e);
                     } catch (InterruptedException e) {
                         // We should handle interruped exception in some manner
                         log.error("Retrieval of resource " + key + " threw an exception", e);

component/portal/src/main/java/org/exoplatform/portal/config/DataStorageImpl.java
@@ -25,6 +25,8 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.exoplatform.commons.cache.future.FutureExoCache;
+import org.exoplatform.commons.cache.future.Loader;
 import org.exoplatform.commons.utils.LazyPageList;
 import org.exoplatform.commons.utils.ListAccess;
 import org.exoplatform.commons.utils.PropertyManager;
@@ -64,11 +66,19 @@ public class DataStorageImpl implements DataStorage {
 
     protected ExoCache<PortalKey, PortalData> portalConfigCache;
 
+    protected FutureExoCache<PortalKey, PortalData, Object> futurePortalConfigCache;
+
     public DataStorageImpl(CacheService cacheService, ModelDataStorage delegate, ListenerService listenerServ) {
         this.delegate = delegate;
         this.listenerServ_ = listenerServ;
 
         this.portalConfigCache = cacheService.getCacheInstance("portal.PortalConfig");
+        this.futurePortalConfigCache = new FutureExoCache<>(new Loader<PortalKey, PortalData, Object>() {
+          @Override
+          public PortalData retrieve(Object context, PortalKey key) throws Exception {
+            return delegate.getPortalConfig(key);
+          }
+        }, portalConfigCache);
     }
 
     /**
@@ -84,15 +94,15 @@ public class DataStorageImpl implements DataStorage {
         // Clear cache entry since this method can be called even to update an existing
         // Portal Config
         PortalKey key = new PortalKey(config.getType(), config.getName());
-        portalConfigCache.remove(key);
         delegate.create(config.build());
+        portalConfigCache.remove(key);
         listenerServ_.broadcast(PORTAL_CONFIG_CREATED, this, config);
     }
 
     public void save(PortalConfig config) throws Exception {
         PortalKey key = new PortalKey(config.getType(), config.getName());
-        portalConfigCache.remove(key);
         delegate.save(config.build());
+        portalConfigCache.remove(key);
         listenerServ_.broadcast(PORTAL_CONFIG_UPDATED, this, config);
     }
 
@@ -279,12 +289,9 @@ public class DataStorageImpl implements DataStorage {
     }
 
     public PortalConfig getPortalConfig(String ownerType, String portalName) throws Exception {
-        PortalKey key = new PortalKey(ownerType, portalName);
-        PortalData data = portalConfigCache.get(key);
-        if (data == null) {
-          data = delegate.getPortalConfig(key);
-        }
-        return data != null ? new PortalConfig(data) : null;
+      PortalKey key = new PortalKey(ownerType, portalName);
+      PortalData data = futurePortalConfigCache.get(null, key);
+      return data != null ? new PortalConfig(data) : null;
     }
 
     public Dashboard loadDashboard(String dashboardId) throws Exception {

component/portal/src/main/java/org/exoplatform/portal/config/NewPortalConfigListener.java
@@ -23,7 +23,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
@@ -56,11 +55,10 @@ import org.exoplatform.portal.mop.importer.NavigationImporter;
 import org.exoplatform.portal.mop.importer.PageImporter;
 import org.exoplatform.portal.mop.importer.PortalConfigImporter;
 import org.exoplatform.portal.mop.navigation.NavigationService;
-import org.exoplatform.portal.mop.page.PageContext;
 import org.exoplatform.portal.mop.page.PageService;
-import org.exoplatform.portal.mop.page.PageServiceWrapper;
 import org.exoplatform.portal.pom.config.POMSession;
 import org.exoplatform.portal.pom.config.POMSessionManager;
+
 import org.gatein.common.logging.Logger;
 import org.gatein.common.logging.LoggerFactory;
 import org.gatein.mop.api.workspace.Workspace;

commit b8da60cb4165e309bb2612c482af6bb9292c4ac9

EXOGTN-2382 : Revert "EXOGTN-2320 Optimize MOP cache for cluster (#149)" (#194)
This reverts commit ffaae9e82cde2a8354ced7939c9ff49e9e94033d.
The commit ffaae9e82cde2a8354ced7939c9ff49e9e94033d introduced a functional regression. The cache updates on MOP does not invalidate cache of others cluster nodes.
Therefore we revert it until we find a better solution.

component/common/src/main/java/org/exoplatform/commons/cache/future/FutureCache.java
@@ -131,7 +131,7 @@ public abstract class FutureCache<K, V, C> {
                         retrieval.current = Thread.currentThread();
                         retrieval.future.run();
                     } catch (Exception e) {
-                        throw new RuntimeException("Retrieval of resource " + key + " threw an exception", e);
+                        log.error("Retrieval of resource " + key + " threw an exception", e);
                     } finally {
                         retrieval.current = null;
                     }
@@ -145,7 +145,7 @@ public abstract class FutureCache<K, V, C> {
                     try {
                         value = retrieval.future.get();
                     } catch (ExecutionException e) {
-                      throw new RuntimeException("Computing of resource " + key + " threw an exception", e);
+                        log.error("Computing of resource " + key + " threw an exception", e.getCause());
                     } catch (InterruptedException e) {
                         // We should handle interruped exception in some manner
                         log.error("Retrieval of resource " + key + " threw an exception", e);

component/portal/src/main/java/org/exoplatform/portal/config/DataStorageImpl.java
@@ -25,8 +25,6 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.exoplatform.commons.cache.future.FutureExoCache;
-import org.exoplatform.commons.cache.future.Loader;
 import org.exoplatform.commons.utils.LazyPageList;
 import org.exoplatform.commons.utils.ListAccess;
 import org.exoplatform.commons.utils.PropertyManager;
@@ -68,20 +66,10 @@ public class DataStorageImpl implements DataStorage {
 
     private final static String CACHE_NAME = "portal.PortalConfig";
 
-    protected FutureExoCache<PortalKey, PortalData, Object> futurePortalConfigCache;
-
     public DataStorageImpl(CacheService cacheService, ModelDataStorage delegate, ListenerService listenerServ) {
         this.delegate = delegate;
         this.listenerServ_ = listenerServ;
-
         this.portalConfigCache = cacheService.getCacheInstance(CACHE_NAME);
-        this.futurePortalConfigCache = new FutureExoCache<>(new Loader<PortalKey, PortalData, Object>() {
-          @Override
-          public PortalData retrieve(Object context, PortalKey key) throws Exception {
-            PortalData portalConfig = delegate.getPortalConfig(key);
-            return portalConfig == null ? PortalData.NULL_OBJECT : portalConfig;
-          }
-        }, portalConfigCache);
     }
 
     /**
@@ -97,15 +85,15 @@ public class DataStorageImpl implements DataStorage {
         // Clear cache entry since this method can be called even to update an existing
         // Portal Config
         PortalKey key = new PortalKey(config.getType(), config.getName());
-        delegate.create(config.build());
         portalConfigCache.remove(key);
+        delegate.create(config.build());
         listenerServ_.broadcast(PORTAL_CONFIG_CREATED, this, config);
     }
 
     public void save(PortalConfig config) throws Exception {
         PortalKey key = new PortalKey(config.getType(), config.getName());
-        delegate.save(config.build());
         portalConfigCache.remove(key);
+        delegate.save(config.build());
         listenerServ_.broadcast(PORTAL_CONFIG_UPDATED, this, config);
     }
 
@@ -292,9 +280,12 @@ public class DataStorageImpl implements DataStorage {
     }
 
     public PortalConfig getPortalConfig(String ownerType, String portalName) throws Exception {
-      PortalKey key = new PortalKey(ownerType, portalName);
-      PortalData data = futurePortalConfigCache.get(null, key);
-      return data != null && data.getKey() != null ? new PortalConfig(data) : null;
+        PortalKey key = new PortalKey(ownerType, portalName);
+        PortalData data = portalConfigCache.get(key);
+        if (data == null) {
+          data = delegate.getPortalConfig(key);
+        }
+        return data != null ? new PortalConfig(data) : null;
     }
 
     public Dashboard loadDashboard(String dashboardId) throws Exception {

component/portal/src/main/java/org/exoplatform/portal/config/NewPortalConfigListener.java
@@ -23,6 +23,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
@@ -55,10 +56,11 @@ import org.exoplatform.portal.mop.importer.NavigationImporter;
 import org.exoplatform.portal.mop.importer.PageImporter;
 import org.exoplatform.portal.mop.importer.PortalConfigImporter;
 import org.exoplatform.portal.mop.navigation.NavigationService;
+import org.exoplatform.portal.mop.page.PageContext;
 import org.exoplatform.portal.mop.page.PageService;
+import org.exoplatform.portal.mop.page.PageServiceWrapper;
 import org.exoplatform.portal.pom.config.POMSession;
 import org.exoplatform.portal.pom.config.POMSessionManager;
-
 import org.gatein.common.logging.Logger;
 import org.gatein.common.logging.LoggerFactory;
 import org.gatein.mop.api.workspace.Workspace;

component/portal/src/main/java/org/exoplatform/portal/pom/config/POMSession.java
@@ -33,6 +33,10 @@ import org.chromattic.api.ChromatticSession;
 import org.chromattic.api.UndeclaredRepositoryException;
 import org.chromattic.api.query.QueryResult;
 import org.chromattic.ext.format.BaseEncodingObjectFormatter;
+import org.exoplatform.commons.chromattic.SessionContext;
+import org.exoplatform.commons.chromattic.SynchronizationListener;
+import org.exoplatform.commons.chromattic.SynchronizationStatus;
+import org.exoplatform.portal.config.NoSuchDataException;
 import org.gatein.common.logging.Logger;
 import org.gatein.common.logging.LoggerFactory;
 import org.gatein.mop.api.Model;
@@ -45,14 +49,6 @@ import org.gatein.mop.core.api.ModelImpl;
 import org.gatein.mop.core.api.workspace.NavigationImpl;
 import org.gatein.mop.core.api.workspace.PageImpl;
 
-import org.exoplatform.commons.chromattic.SessionContext;
-import org.exoplatform.commons.chromattic.SynchronizationListener;
-import org.exoplatform.commons.chromattic.SynchronizationStatus;
-import org.exoplatform.portal.config.NoSuchDataException;
-import org.exoplatform.portal.pom.config.cache.CacheableDataTask;
-import org.exoplatform.portal.pom.config.cache.DataCache;
-import org.exoplatform.portal.pom.config.cache.DataCacheContext;
-
 /**
  * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
  * @version $Revision$
@@ -63,7 +59,7 @@ public final class POMSession {
     private static final Logger log = LoggerFactory.getLogger(POMSession.class);
 
     /** . */
-    private static final Map<ObjectType<?>, Class<?>> mapping = new HashMap<ObjectType<?>, Class<?>>();
+    private static final Map<ObjectType<?>, Class> mapping = new HashMap<ObjectType<?>, Class>();
 
     static {
         mapping.put(ObjectType.PAGE, PageImpl.class);
@@ -107,11 +103,18 @@ public final class POMSession {
         this.context = context;
     }
 
-    public Object getFromCache(Serializable key, DataCacheContext cacheContext) {
+    public Object getFromCache(Serializable key) {
         if (isModified()) {
             throw new IllegalStateException("Cannot read object in shared cache from a modified session");
         }
-        return mgr.cacheGet(key, cacheContext);
+        return mgr.cacheGet(key);
+    }
+
+    public void putInCache(Serializable key, Object value) {
+        if (isModified()) {
+            throw new IllegalStateException("Cannot put object in shared cache from a modified session");
+        }
+        mgr.cachePut(key, value);
     }
 
     public void scheduleForEviction(Serializable key) {
@@ -249,7 +252,6 @@ public final class POMSession {
 
         // Temporary work around, to fix in MOP and then remove
         ChromatticSession session = context.getSession();
-        @SuppressWarnings("unchecked")
         Class<O> mappedClass = (Class<O>) mapping.get(type);
         return session.createQueryBuilder(mappedClass).where(statement).get().objects((long) offset, (long) limit);
     }

component/portal/src/main/java/org/exoplatform/portal/pom/config/POMSessionManager.java
@@ -22,16 +22,11 @@ package org.exoplatform.portal.pom.config;
 import java.io.Serializable;
 import java.lang.reflect.UndeclaredThrowableException;
 
-import org.exoplatform.commons.cache.future.FutureExoCache;
-import org.exoplatform.commons.cache.future.Loader;
 import org.exoplatform.commons.chromattic.ChromatticLifeCycle;
 import org.exoplatform.commons.chromattic.ChromatticManager;
 import org.exoplatform.commons.chromattic.SessionContext;
 import org.exoplatform.commons.scope.ScopedKey;
-import org.exoplatform.portal.pom.config.cache.CacheableDataTask;
 import org.exoplatform.portal.pom.config.cache.DataCache;
-import org.exoplatform.portal.pom.config.cache.DataCacheContext;
-import org.exoplatform.portal.pom.config.cache.NullObject;
 import org.exoplatform.portal.pom.config.cache.PortalNamesCache;
 import org.exoplatform.portal.pom.data.OwnerKey;
 import org.exoplatform.portal.pom.data.PortalKey;
@@ -63,9 +58,6 @@ public class POMSessionManager implements Startable {
     private final ExoCache<ScopedKey<?>, Object> cache;
 
     /** . */
-    private final FutureExoCache<ScopedKey<?>, Object, DataCacheContext> futureCache;
-
-    /** . */
     final ChromatticManager manager;
 
     /** . */
@@ -74,17 +66,14 @@ public class POMSessionManager implements Startable {
     /** . */
     private final TaskExecutionDecorator executor;
 
+    /** . */
+    private final RepositoryService repositoryService;
+
     public POMSessionManager(RepositoryService repositoryService, ChromatticManager manager, CacheService cacheService) {
         //
+        this.repositoryService = repositoryService;
         this.manager = manager;
         this.cache = cacheService.getCacheInstance(CACHE_NAME);
-        futureCache = new FutureExoCache<>(new Loader<ScopedKey<?>, Object, DataCacheContext>() {
-          @Override
-          public Object retrieve(DataCacheContext dataCacheContext, ScopedKey<?> key) throws Exception {
-            Object result = dataCacheContext.execute();
-            return result == null ? NullObject.get() : result;
-          }
-        }, cache);
         this.pomService = null;
         this.executor = new PortalNamesCache(new DataCache(new ExecutorDispatcher()));
     }
@@ -93,11 +82,23 @@ public class POMSessionManager implements Startable {
         return configurator;
     }
 
-    public Object cacheGet(Serializable key, DataCacheContext cacheContext) {
+    public void cachePut(Serializable key, Object value) {
         ScopedKey<?> globalKey = ScopedKey.create(key);
 
         //
-        Object value = futureCache.get(cacheContext, globalKey);
+        if (log.isTraceEnabled()) {
+            log.trace("Updating cache key=" + globalKey + " with value=" + value);
+        }
+
+        //
+        cache.put(globalKey, value);
+    }
+
+    public Object cacheGet(Serializable key) {
+        ScopedKey globalKey = ScopedKey.create(key);
+
+        //
+        Object value = cache.get(globalKey);
 
         //
         if (log.isTraceEnabled()) {
@@ -105,7 +106,7 @@ public class POMSessionManager implements Startable {
         }
 
         //
-        return value instanceof NullObject ? null : value;
+        return value;
     }
 
     public void cacheRemove(Serializable key) {

component/portal/src/main/java/org/exoplatform/portal/pom/config/TaskExecutionDecorator.java
@@ -32,11 +32,7 @@ public class TaskExecutionDecorator implements TaskExecutor {
     }
 
     public <V> V execute(POMSession session, POMTask<V> task) throws Exception {
-        return executeWithoutCache(session, task);
-    }
-
-    public <V> V executeWithoutCache(POMSession session, POMTask<V> task) throws Exception {
-      return next.execute(session, task);
+        return next.execute(session, task);
     }
 
     public <E extends TaskExecutor> E getDecorator(Class<E> decoratorClass) {

component/portal/src/main/java/org/exoplatform/portal/pom/config/cache/DataCache.java
@@ -38,13 +38,18 @@ public class DataCache extends TaskExecutionDecorator {
     /** . */
     private final Logger log = LoggerFactory.getLogger(DataCache.class);
 
+    /** . */
+    private final AtomicLong readCount = new AtomicLong();
+
+    /** . */
+    private boolean cluster = ExoContainer.getProfiles().contains("cluster");
+
     public DataCache(TaskExecutor next) {
         super(next);
     }
 
     public <V> V execute(POMSession session, POMTask<V> task) throws Exception {
         if (task instanceof CacheableDataTask) {
-            @SuppressWarnings("unchecked")
             CacheableDataTask<?, V> loadTask = (CacheableDataTask<?, V>) task;
             switch (loadTask.getAccessMode()) {
                 case READ:
@@ -95,14 +100,14 @@ public class DataCache extends TaskExecutionDecorator {
 
         //
         if (!session.isModified()) {
-            Object o = session.getFromCache(key, new DataCacheContext(this, task, session));
+            Object o = session.getFromCache(key);
             if (log.isTraceEnabled()) {
                 log.trace("Retrieved " + o + " for key " + key);
             }
 
             V v = null;
             if (o != null) {
-                if (o instanceof NullObject) {
+                if (o == NullObject.get()) {
                     if (log.isTraceEnabled()) {
                         log.trace("Returning null as found null object marker");
                     }
@@ -116,7 +121,45 @@ public class DataCache extends TaskExecutionDecorator {
                     }
                 }
             }
-            return v;
+
+            //
+            if (v != null) {
+                if (log.isTraceEnabled()) {
+                    log.trace("Returning object " + v + " for key " + key);
+                }
+                return v;
+            } else {
+                readCount.incrementAndGet();
+
+                //
+                if (log.isTraceEnabled()) {
+                    log.trace("Object not found in cache for key " + key + " about to retrieve it");
+                }
+
+                //
+                v = super.execute(session, task);
+                if (log.isTraceEnabled()) {
+                    log.trace("Retrieved object " + v + " key " + key + " that will be returned");
+                }
+
+                //
+                if (!session.isModified()) {
+                    if (v == null) {
+                        if (log.isTraceEnabled()) {
+                            log.trace("Updating cache with null object for key " + key);
+                        }
+                        session.putInCache(key, NullObject.get());
+                    } else {
+                        if (log.isTraceEnabled()) {
+                            log.trace("Updating cache with object " + v + " for key " + key);
+                        }
+                        session.putInCache(key, v);
+                    }
+                }
+
+                //
+                return v;
+            }
         } else {
             if (log.isTraceEnabled()) {
                 log.trace("Session was modified, object for key " + key + " is directly retrieved");
@@ -125,5 +168,9 @@ public class DataCache extends TaskExecutionDecorator {
             //
             return super.execute(session, task);
         }
+    }
+
+    public long getReadCount() {
+        return readCount.longValue();
     }
 }

component/portal/src/main/java/org/exoplatform/portal/pom/config/cache/DataCacheContext.java
@@ -1,35 +1,0 @@
-package org.exoplatform.portal.pom.config.cache;
-
-import org.exoplatform.portal.pom.config.POMSession;
-import org.exoplatform.portal.pom.config.POMTask;
-import org.exoplatform.portal.pom.config.TaskExecutionDecorator;
-
-public class DataCacheContext {
-  private TaskExecutionDecorator taskExecutionDecorator;
-
-  private POMTask<?>             task;
-
-  private POMSession             session;
-
-  public DataCacheContext(TaskExecutionDecorator taskExecutionDecorator, POMTask<?> task, POMSession session) {
-    this.taskExecutionDecorator = taskExecutionDecorator;
-    this.task = task;
-    this.session = session;
-  }
-
-  public POMSession getSession() {
-    return session;
-  }
-
-  public POMTask<?> getTask() {
-    return task;
-  }
-
-  public TaskExecutionDecorator getTaskExecutionDecorator() {
-    return taskExecutionDecorator;
-  }
-
-  public Object execute() throws Exception {
-    return taskExecutionDecorator.executeWithoutCache(session, task);
-  }
-}

component/portal/src/main/java/org/exoplatform/portal/pom/config/cache/PortalNamesCache.java
@@ -19,6 +19,8 @@
 
 package org.exoplatform.portal.pom.config.cache;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.exoplatform.commons.utils.LazyPageList;
@@ -46,18 +48,14 @@ public class PortalNamesCache extends TaskExecutionDecorator {
         if (!session.isModified()) {
             if (task instanceof SearchTask.FindSiteKey) {
                 SearchTask.FindSiteKey find = (SearchTask.FindSiteKey) task;
-                Object objectFromCache = session.getFromCache(find.getKey(), new DataCacheContext(this, task, session));
-                if (objectFromCache == null) {
-                    return null;
+                List<PortalKey> data = (List<PortalKey>) session.getFromCache(find.getKey());
+                if (data == null) {
+                    V result = super.execute(session, task);
+                    LazyPageList<PortalKey> list = (LazyPageList<PortalKey>) result;
+                    session.putInCache(find.getKey(), Collections.unmodifiableList(new ArrayList<PortalKey>(list.getAll())));
+                    return result;
                 } else {
-                    if (objectFromCache instanceof LazyPageList) {
-                      return (V) objectFromCache;
-                    } else if (objectFromCache instanceof List) {
-                      List<PortalKey> data = (List<PortalKey>) objectFromCache;
-                      return (V) new LazyPageList<PortalKey>(new ListAccessImpl<PortalKey>(PortalKey.class, data), 10);
-                    } else {
-                      throw new IllegalStateException("Unknown data type " + objectFromCache.getClass());
-                    }
+                    return (V) new LazyPageList<PortalKey>(new ListAccessImpl<PortalKey>(PortalKey.class, data), 10);
                 }
             } else if (task instanceof PortalConfigTask.Save || task instanceof PortalConfigTask.Remove) {
                 V result = super.execute(session, task);

component/common/src/test/java/org/exoplatform/commons/cache/future/GetTestCase.java
@@ -54,16 +54,11 @@ public class GetTestCase extends TestCase {
     public void testThrowException() {
         FutureMap<String, String, Callable<String>> futureCache = new FutureMap<String, String, Callable<String>>(
                 new StringLoader());
-        try {
-          futureCache.get(new Callable<String>() {
-              public String call() throws Exception {
-                  throw new Exception("DON'T FREAK OUT");
-              }
-          }, "foo");
-          fail("An exception should be thrown");
-        } catch (Exception e) {
-          // Expected
-        }
+        Assert.assertEquals(null, futureCache.get(new Callable<String>() {
+            public String call() throws Exception {
+                throw new Exception("DON'T FREAK OUT");
+            }
+        }, "foo"));
         Assert.assertFalse(futureCache.data.containsKey("foo"));
     }
 

component/portal/src/test/java/org/exoplatform/portal/config/TestUserPortalConfigService.java
@@ -104,14 +104,12 @@ public class TestUserPortalConfigService extends AbstractConfigTest {
     public TestUserPortalConfigService(String name) {
         super(name);
 
-        setForceContainerReload(true);
         //
         registered = false;
     }
 
     @Override
     protected void setUp() throws Exception {
-        super.setUp();
         Listener listener = new Listener() {
             @Override
             public void onEvent(Event event) throws Exception {
@@ -564,6 +562,22 @@ public class TestUserPortalConfigService extends AbstractConfigTest {
                 assertTrue(((Application) container.getChildren().get(1)).getType() == ApplicationType.PORTLET);
 
                 groupHandler.removeGroup(group, true);
+            }
+        }.execute(null);
+    }
+
+    public void testCacheUserPortalConfig() {
+        new UnitTest() {
+            public void execute() throws Exception {
+                mgr.clearCache();
+                DataCache cache = mgr.getDecorator(DataCache.class);
+                long readCount0 = cache.getReadCount();
+                userPortalConfigSer_.getUserPortalConfig("classic", null);
+                long readCount1 = cache.getReadCount();
+                assertTrue(readCount1 > readCount0);
+                userPortalConfigSer_.getUserPortalConfig("classic", null);
+                long readCount2 = cache.getReadCount();
+                assertEquals(readCount1, readCount2);
             }
         }.execute(null);
     }