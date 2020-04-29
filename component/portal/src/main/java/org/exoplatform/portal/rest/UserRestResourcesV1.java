6f6c2229d99fa32cf0b4c45ac6f80da3ccf8445c
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-04-29 23:16:17 +0100

    Add REST endpoint to change user password

--- component/portal/src/main/java/org/exoplatform/portal/rest/UserRestResourcesV1.java
+++ component/portal/src/main/java/org/exoplatform/portal/rest/UserRestResourcesV1.java
@@ -1,30 +1,45 @@
 package org.exoplatform.portal.rest;
 
 import java.util.Collection;
+import java.util.regex.Pattern;
 
 import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
 
+import org.apache.commons.lang3.StringUtils;
+
+import org.exoplatform.commons.utils.PropertyManager;
 import org.exoplatform.portal.config.UserACL;
-import org.exoplatform.services.organization.Membership;
-import org.exoplatform.services.organization.OrganizationService;
-import org.exoplatform.services.organization.User;
+import org.exoplatform.services.organization.*;
 import org.exoplatform.services.rest.resource.ResourceContainer;
 import org.exoplatform.services.security.ConversationState;
 
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
+import io.swagger.jaxrs.PATCH;
 
 @Path("v1/users")
 public class UserRestResourcesV1 implements ResourceContainer {
+
+  public static final String  PASSWORD_UNKNOWN_ERROR_CODE            = "PASSWORD_UNKNOWN_ERROR";
+
+  public static final String  PASSWORD_REGEX_ERROR_CODE              = "PASSWORD_REGEX_ERROR";
+
+  public static final String  PASSWORD_MAX_LENGTH_ERROR_CODE         = "PASSWORD_MAX_LENGTH";
+
+  public static final String  PASSWORD_MIN_LENGTH_ERROR_CODE         = "PASSWORD_MIN_LENGTH";
+
+  public static final String  PASSWORD_CUSTOM_VALIDATOR_MIN          = "gatein.validators.password.length.min";
+
+  public static final String  PASSWORD_CUSTOM_VALIDATOR_MAX          = "gatein.validators.password.length.max";
+
+  public static final String  PASSWORD_CUSTOM_VALIDATOR_REGEXP       = "gatein.validators.password.regexp";
+
+  public static final String  PASSWORD_CUSTOM_VALIDATOR_REGEXP_ERROR = "gatein.validators.password.format.message";
+
+  public static final String  USER_NOT_FOUND_ERROR_CODE              = "USER_NOT_FOUND";
+
+  public static final String  WRONG_USER_PASSWORD_ERROR_CODE         = "WRONG_USER_PASSWORD";
 
   private OrganizationService organizationService;
 
@@ -71,11 +86,72 @@ public class UserRestResourcesV1 implements ResourceContainer {
                                                        false);
 
     Collection<Membership> memberships = organizationService.getMembershipHandler().findMembershipsByUser(id);
-    if(memberships != null) {
-      userRestEntity.setPlatformAdministrator(memberships.stream().anyMatch(membership ->
-              membership.getGroupId().equals(userACL.getAdminGroups())));
+    if (memberships != null) {
+      userRestEntity.setPlatformAdministrator(memberships.stream()
+                                                         .anyMatch(membership -> membership.getGroupId()
+                                                                                           .equals(userACL.getAdminGroups())));
     }
 
     return Response.ok(userRestEntity, MediaType.APPLICATION_JSON).build();
+  }
+
+  @PATCH
+  @RolesAllowed("users")
+  @Path("{id}/changePassword")
+  @ApiOperation(value = "Changes user password", httpMethod = "POST", response = Response.class)
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "Request fulfilled"),
+      @ApiResponse(code = 401, message = "User not authorized to call this endpoint"),
+      @ApiResponse(code = 404, message = "User not found"),
+      @ApiResponse(code = 500, message = "Internal server error") })
+  public Response changePassword(@ApiParam(value = "username to change his password", required = true) @PathParam("id") String username,
+                                 @ApiParam(value = "Current user password", required = true) @FormParam("currentPassword") String currentPassword,
+                                 @ApiParam(value = "New user password", required = true) @FormParam("newPassword") String newPassword) {
+    boolean isAdmin = userACL.isSuperUser() || userACL.isUserInGroup(userACL.getAdminGroups());
+    boolean isSameUser = ConversationState.getCurrent().getIdentity().getUserId().equals(username);
+    if (!isAdmin && !isSameUser) {
+      return Response.status(Response.Status.UNAUTHORIZED).build();
+    }
+
+    try {
+      UserHandler userHandler = organizationService.getUserHandler();
+      User user = userHandler.findUserByName(username);
+      if (user == null) {
+        return Response.serverError().entity(USER_NOT_FOUND_ERROR_CODE).build();
+      }
+
+      if (isSameUser && !userHandler.authenticate(username, currentPassword)) {
+        return Response.serverError().entity(WRONG_USER_PASSWORD_ERROR_CODE).build();
+      }
+
+      String errorMessage = checkCustomPasswordValidators(newPassword);
+      if (StringUtils.isNotBlank(errorMessage)) {
+        return Response.serverError().entity(errorMessage).build();
+      }
+
+      user.setPassword(newPassword);
+      userHandler.saveUser(user, false);
+
+      return Response.noContent().build();
+    } catch (Exception e) {
+      return Response.serverError().entity(PASSWORD_UNKNOWN_ERROR_CODE + ":" + e.getMessage()).build();
+    }
+  }
+
+  private String checkCustomPasswordValidators(String newPassword) {
+    String minLengthString = PropertyManager.getProperty(PASSWORD_CUSTOM_VALIDATOR_MIN);
+    if (StringUtils.isNotBlank(minLengthString) && newPassword.length() < Integer.parseInt(minLengthString)) {
+      return PASSWORD_MIN_LENGTH_ERROR_CODE + ":" + minLengthString;
+    }
+    String maxLengthString = PropertyManager.getProperty(PASSWORD_CUSTOM_VALIDATOR_MAX);
+    if (StringUtils.isNotBlank(maxLengthString) && newPassword.length() > Integer.parseInt(maxLengthString)) {
+      return PASSWORD_MAX_LENGTH_ERROR_CODE + ":" + maxLengthString;
+    }
+    String passwordRegex = PropertyManager.getProperty(PASSWORD_CUSTOM_VALIDATOR_REGEXP);
+    if (StringUtils.isNotBlank(passwordRegex) && !Pattern.matches(passwordRegex, passwordRegex)) {
+      String passwordRegexError = PropertyManager.getProperty(PASSWORD_CUSTOM_VALIDATOR_REGEXP_ERROR);
+      return PASSWORD_REGEX_ERROR_CODE + ":" + passwordRegexError;
+    }
+    return null;
   }
 }
