81f159bb5a23bd6f6d10fba608f0078569f78232
Moez Diha <38471028+mdiha@users.noreply.github.com>
2020-07-23 09:57:25 +0100

    MAINT-26638: Make sure that the company branding logo dosn't move on mouse hover. (#60)

--- portlet/exoadmin/src/main/webapp/company-branding-app/components/ExoCompanyBranding.vue
+++ portlet/exoadmin/src/main/webapp/company-branding-app/components/ExoCompanyBranding.vue
@@ -27,12 +27,14 @@
         <div class="clearfix">
           <div v-show="showLogo" class="pull-left">
             <div id="previewLogo" class="previewLogo">
-              <div class="btnAction">
-                <i v-if="removeLogoButtonDisplayed" :title="$t('branding.message.restore.logo')" class="fas fa-trash-alt iconDelete" rel="tooltip" data-placement="bottom" @click="removeLogo"></i>
-                <i v-else :title="$t('branding.message.change.logo')" class="uiIconEdit iconEdit" rel="tooltip" data-placement="bottom" @click="showLogo = false"></i>
-              </div>
               <div class="logoPreviewImage">
                 <img id="previewLogoImg" :src="logoPreview" class="previewImg">
+              </div>
+              <div class="btnActionContainer">
+                <div class="btnAction">
+                  <i v-if="removeLogoButtonDisplayed" :title="$t('branding.message.restore.logo')" class="fas fa-trash-alt iconDelete" rel="tooltip" data-placement="bottom" @click="removeLogo"></i>
+                  <i v-else :title="$t('branding.message.change.logo')" class="uiIconEdit iconEdit" rel="tooltip" data-placement="bottom" @click="showLogo = false"></i>
+                </div>
               </div>
             </div>
           </div>
