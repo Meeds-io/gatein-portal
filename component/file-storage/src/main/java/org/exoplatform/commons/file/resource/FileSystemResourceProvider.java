437004cbc04b9b044bcdd109af54b5cc748c6298
Romain Dénarié <romain.denarie@exoplatform.com>
2020-09-30 14:53:16 +0200

    Prior to this change, when a binary content is not accessible, there is no error log related. The binary content can be not accessible because of data corruption, or because it was quarantined by an antivirus. This change will allow to improve the message in logs when a binary content is not accessible.

--- component/file-storage/src/main/java/org/exoplatform/commons/file/resource/FileSystemResourceProvider.java
+++ component/file-storage/src/main/java/org/exoplatform/commons/file/resource/FileSystemResourceProvider.java
@@ -132,6 +132,12 @@ public class FileSystemResourceProvider implements BinaryProvider {
     try {
       return new FileInputStream(getFile(name));
     } catch (IOException e) {
+      try {
+        log.error("Unable to read binary content from disk for file "+getFile(name).getPath() +". The binary content is not "
+                      + "accessible, it was removed, or may have been quarantined by an antivirus.",e);
+      } catch (IOException ioException) {
+        log.error("Unable to get file object for name "+name,ioException);
+      }
     }
     return null;
   }
