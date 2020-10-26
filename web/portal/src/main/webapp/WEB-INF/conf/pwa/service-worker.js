893c86a92be52a82c66fb09b4c186198fba9f015
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-10-26 18:16:56 +0100

    TASK-35860 Improve front end performances (#86)
    * Add Default Manifest of portal
    * Make left navigation loaded when opening drawer
    * Make basic portal template having empty applications
    * Add missing workbox modules
    * Add Vue applications DOM Caching in service worker
    * Differ Hamburger menu resources loading from page blocking resources
    * Improve Service Worker by adding parameterable variables
    * Preload Font files for better page loading performances
    
    This PR defines a Service worker to manage efficiently resources caching and prepare to PWA feature for making Portals accessible in offline mode.
    
    In Service Worker, we manages the DOM parts (portlets or part of portlets) loading optimization, by allowing to render the last version of a chunck of html at the same time than rendering the page. In fact when the page is loaded, before returning the HTML to browser in order to display it, the Service Worker will search for custom HTML tag `<v-cacheable-dom-app cache-id="CACHE_ID"></v-cacheable-dom-app>` and search whether a cache key `CACHE_ID` exists in browser cache or not, if exists, it will be replaced by the cached DOM, else, the tag will be ignored by the browser anyway. After displaying the Application, the application itself will have to put DOM inside browser cache to use it next loading time.

---
+++ web/portal/src/main/webapp/WEB-INF/conf/pwa/service-worker.js
@@ -0,0 +1,186 @@
+importScripts('/eXoResources/javascript/workbox-5.1.4/workbox-sw.js');
+
+const cachePrefix = 'portal-pwa-resources';
+const assetsVersion = '@assets-version@';
+const siteName = '@site-name@';
+const development = @development@;
+
+workbox.setConfig({
+  debug: development,
+  modulePathPrefix: '/eXoResources/javascript/workbox-5.1.4/'
+});
+
+workbox.loadModule('workbox-strategies');
+
+workbox.core.setCacheNameDetails({
+  prefix: cachePrefix,
+  suffix: assetsVersion,
+  precache: 'preload',
+});
+
+const cssCacheName = `${cachePrefix}-css-${assetsVersion}`;
+const jsCacheName = `${cachePrefix}-js-${assetsVersion}`;
+const fontCacheName = `${cachePrefix}-font-${assetsVersion}`;
+const bundleCacheName = `${cachePrefix}-bundle-${assetsVersion}`;
+const manifestCacheName = `${cachePrefix}-manifest-${assetsVersion}`;
+
+const imageCacheName = `${cachePrefix}-image`;
+const domCacheName = `${cachePrefix}-dom`;
+
+const cachesWhiteList = [
+  imageCacheName,
+  domCacheName,
+  workbox.core.cacheNames.precache,
+  cssCacheName,
+  jsCacheName,
+  fontCacheName,
+  bundleCacheName,
+  manifestCacheName];
+
+workbox.routing.registerRoute(
+  new RegExp('.*manifest.json$'),
+  new workbox.strategies.CacheFirst({
+    cacheName: manifestCacheName,
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*\\.(ttf|woff|woff2|otf|otf|ttc)($|\\?.*|\\#.*)'),
+  new workbox.strategies.CacheFirst({
+    cacheName: fontCacheName,
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*\\.css($|\\?.*|\\#.*)'),
+  new workbox.strategies.CacheFirst({
+    cacheName: cssCacheName,
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*/rest/v1/platform/branding/css.*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: cssCacheName,
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*\\.js($|\\?.*|\\#.*)'),
+  new workbox.strategies.CacheFirst({
+    cacheName: jsCacheName,
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*/i18n/bundle/.*\\.json'),
+  new workbox.strategies.CacheFirst({
+    cacheName: bundleCacheName,
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*\\.(?:png|jpg|jpeg|svg|gif|ico)'),
+  new workbox.strategies.CacheFirst({
+    cacheName: imageCacheName,
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*/rest/.*/social/.*/avatar.*'),
+  new workbox.strategies.CacheFirst({
+    cacheName: imageCacheName,
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*/rest/.*/social/.*/banner.*'),
+  new workbox.strategies.CacheFirst({
+    cacheName: imageCacheName,
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*/dom-cache.*'),
+  new workbox.strategies.CacheOnly({
+    cacheName: domCacheName,
+  }),
+);
+
+const cacheableDOM = new workbox.cacheableResponse.CacheableResponse({
+  statuses: [200],
+  headers: {
+    'Content-Type': ['text/html;charset=UTF-8', 'text/html'],
+  },
+});
+
+const handleDOMResponse = (event) => {
+  return fetch(event.request)
+    .then((response) => {
+      if (cacheable.isResponseCacheable(response)) {
+        return response.text()
+          .then(html => {
+          });
+      }
+      return response;
+    });
+};
+
+const domMatcher = ({url, request, event}) => {
+  const pathname = url.pathname;
+  return (
+          pathname.includes(`/${siteName}`)
+          || pathname.includes('/g/:')
+          || pathname.includes('/u/:')
+         )
+         && !pathname.includes('/rest/')
+         && !pathname.includes('.js')
+         && !pathname.includes('.css');
+};
+
+const domHandler = async ({url, request, event, params}) => {
+  const response = await fetch(request);
+  let html = await response.text();
+
+  try {
+    const cacheableDOMs = [...html.matchAll(/<v-cacheable-dom-app([ \t\r\n]*)cache-id="(.*)"([ \t\r\n]*)(\/>|>[ \t\r\n]*<\/v-cacheable-dom-app>)/g)];
+    if(cacheableDOMs.length) {
+      const domCache = await self.caches.open(domCacheName);
+      for (let index in cacheableDOMs) {
+        const cacheableDOM = cacheableDOMs[index];
+        const domToReplace = cacheableDOM[0];
+        const cacheId = cacheableDOM[2];
+        const domCacheEntry = await domCache.match(`/dom-cache?id=${cacheId}`);
+        if (domCacheEntry) {
+          const htmlAppPart = await domCacheEntry.text();
+          html = html.replace(domToReplace, htmlAppPart);
+        }
+      }
+    }
+  } catch(e) {
+    console.error('Error while treating DOM caches of URL', url, e);
+  }
+  return new Response(html, {
+    headers: {'content-type': 'text/html;charset=UTF-8'},
+  });
+};
+
+workbox.routing.registerRoute(domMatcher, domHandler);
+
+@extended-service-worker-parts@
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+this.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cachesWhiteList.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
