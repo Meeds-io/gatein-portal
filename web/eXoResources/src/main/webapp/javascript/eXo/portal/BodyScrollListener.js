2877be3fd96ad002a02aa06ed7410c726eb15c52
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-02-08 21:22:24 +0100

    TASK-40054 Enhance document scrollbar when opening Analytics Drawer (#164)

--- web/eXoResources/src/main/webapp/javascript/eXo/portal/BodyScrollListener.js
+++ web/eXoResources/src/main/webapp/javascript/eXo/portal/BodyScrollListener.js
@@ -4,6 +4,23 @@ $(document).ready(() => {
     return;
   }
   function controlBodyScrollClass() {
+    // Add a style to define scrollbar width
+    let styleElement;
+    if ($('#bodyScrollStyle').length) {
+      styleElement = $('#bodyScrollStyle')[0];
+    } else {
+      styleElement = document.createElement('style');
+      styleElement.id = 'bodyScrollStyle';
+      styleElement.setAttribute('type', 'text/css');
+      document.head.append(styleElement);
+    }
+
+    const scrollbarWidth = parseInt(window.innerWidth - document.body.offsetWidth);
+    styleElement.textContent = `
+    .hide-scroll.with-scroll {
+      margin-right: ${scrollbarWidth}px !important;
+    }`;
+
     if ($('body').height() > ($(window).height() + 1)) {
       $('body').addClass('with-scroll');
       $('body').removeClass('no-scroll');
@@ -14,14 +31,5 @@ $(document).ready(() => {
   }
   $(window).resize(controlBodyScrollClass);
   controlBodyScrollClass();
-
-  // Add a style to define scrollbar width
-  const scrollbarWidth = parseInt(window.innerWidth - document.body.offsetWidth);
-  const styleElement = document.createElement('style');
-  styleElement.textContent = `
-  .hide-scroll.with-scroll {
-    margin-right: ${scrollbarWidth}px !important;
-  }`;
-  styleElement.setAttribute('type', 'text/css');
-  document.head.append(styleElement);
+  document.addEventListener('displayTopBarLoading', controlBodyScrollClass);
 })
