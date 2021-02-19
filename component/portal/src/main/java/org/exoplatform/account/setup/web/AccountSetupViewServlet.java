8f1856452eb06489c97b175ed570f3c45c08d8e0
Romain Dénarié <romain.denarie@exoplatform.com>
2021-02-19 14:06:19 +0100

    Task-40783 : When using initial account wizard, the user password is exposed in a GET request (#173)
    Before this fix, when we use the initial account wizard, we make a GET request on /portal/login to automatically log the user. The password is added as query String of this request and so exposed in access log.
    This fix moves the AccountSetup servlet from commons to portal, and logs the user directly after form submit without any additionnal request, so that the password is not exposed.

---
+++ component/portal/src/main/java/org/exoplatform/account/setup/web/AccountSetupViewServlet.java
@@ -0,0 +1,37 @@
+package org.exoplatform.account.setup.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.exoplatform.container.PortalContainer;
+
+/**
+ * @author <a href="fbradai@exoplatform.com">Fbradai</a>
+ */
+public class AccountSetupViewServlet extends HttpServlet {
+
+  private final static String AS_JSP_RESOURCE    = "/WEB-INF/jsp/welcome-screens/accountSetup.jsp";
+
+  private AccountSetupService accountSetupService;
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    accountSetupService = PortalContainer.getInstance().getComponentInstanceOfType(AccountSetupService.class);
+
+    if (accountSetupService.mustSkipAccountSetup()) {
+      response.sendRedirect("/");
+    } else {
+      getServletContext().getRequestDispatcher(AS_JSP_RESOURCE).forward(request, response);
+    }
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    doPost(request, response);
+  }
+
+}
---
+++ component/web/security/src/main/java/org/exoplatform/account/setup/web/AccountSetupFilter.java
@@ -0,0 +1,49 @@
+package org.exoplatform.account.setup.web;
+
+import java.io.IOException;
+
+import org.exoplatform.web.filter.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.PortalContainer;
+
+/**
+ * @author <a href="fbradai@exoplatform.com">Fbradai</a>
+ */
+public class AccountSetupFilter implements Filter {
+  private static final String PLF_PLATFORM_EXTENSION_SERVLET_CTX = "/portal";
+
+  private static final String ACCOUNT_SETUP_SERVLET              = "/accountSetup";
+  private static final String ACCOUNT_SETUP_ACTION              = "/accountSetupAction";
+  
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+    HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+    ExoContainer container = PortalContainer.getInstance();
+    AccountSetupService accountSetupService = container.getComponentInstanceOfType(AccountSetupService.class);
+
+    boolean setupDone = accountSetupService.mustSkipAccountSetup();
+
+    String requestUri = httpServletRequest.getRequestURI();
+    boolean isRestUri = requestUri.contains(container.getContext().getRestContextName());
+    boolean isAccountSetupUri = requestUri.equals(PLF_PLATFORM_EXTENSION_SERVLET_CTX+ACCOUNT_SETUP_SERVLET) ||
+        requestUri.equals(PLF_PLATFORM_EXTENSION_SERVLET_CTX+ACCOUNT_SETUP_ACTION);
+    if (!isAccountSetupUri && !setupDone && !isRestUri) {
+      ServletContext platformExtensionContext = httpServletRequest.getSession()
+                                                                  .getServletContext()
+                                                                  .getContext(PLF_PLATFORM_EXTENSION_SERVLET_CTX);
+      platformExtensionContext.getRequestDispatcher(ACCOUNT_SETUP_SERVLET).forward(httpServletRequest, httpServletResponse);
+      return;
+    }
+    chain.doFilter(request, response);
+  }
+}
