827ab8f6acadc4fe0ad4e13bebdcec880e5d13fd
Romain Dénarié <romain.denarie@exoplatform.com>
2021-05-11 15:38:55 +0200

    Update method visbility

--- component/web/security/src/main/java/org/exoplatform/web/login/recovery/ChangePasswordConnector.java
+++ component/web/security/src/main/java/org/exoplatform/web/login/recovery/ChangePasswordConnector.java
@@ -4,6 +4,6 @@ import org.exoplatform.container.component.BaseComponentPlugin;
 
 public abstract class ChangePasswordConnector  extends BaseComponentPlugin {
   
-  abstract void changePassword(final String username, final String password) throws Exception;
+  protected abstract void changePassword(final String username, final String password) throws Exception;
   protected abstract boolean isAllowChangeExternalPassword();
 }
