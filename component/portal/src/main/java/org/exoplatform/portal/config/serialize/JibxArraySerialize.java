8da0d724b75130f790b3e714168a69d8bf5fdb7a
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-05-27 19:08:40 +0100

    Revert "TASK-26265 Define Portal API module (#216)"
    This reverts commit b4c78e4e5ccdb72ed80c1ec9547f12dc4419c622.

--- component/portal/src/main/java/org/exoplatform/portal/config/serialize/JibxArraySerialize.java
+++ component/portal/src/main/java/org/exoplatform/portal/config/serialize/JibxArraySerialize.java
@@ -23,15 +23,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import org.exoplatform.portal.config.UserACL;
+
 /**
  * Created by The eXo Platform SARL Author : Nhu Dinh Thuan nhudinhthuan@exoplatform.com Jun 2, 2007
  */
 public class JibxArraySerialize {
-  
-    /**
-     * {@code "Nobody"} is equivalent to empty list of permissions.
-     */
-    public static final String NOBODY = "Nobody";
 
     public static String serializeStringArray(String[] values) {
         if (values == null || values.length == 0) {
@@ -76,7 +73,7 @@ public class JibxArraySerialize {
 
     public static String serializePermissions(String[] values) {
         if (values == null || values.length == 0) {
-            return NOBODY;
+            return UserACL.NOBODY;
         } else {
             StringBuilder builder = new StringBuilder();
             for (int i = 0; i < values.length; i++) {
@@ -90,7 +87,7 @@ public class JibxArraySerialize {
                     }
                 }
             }
-            return builder.length() == 0 ? NOBODY : builder.toString();
+            return builder.length() == 0 ? UserACL.NOBODY : builder.toString();
         }
     }
 
@@ -99,7 +96,7 @@ public class JibxArraySerialize {
             return new String[0];
         } else {
             text = text.trim();
-            if (text.length() == 0 || NOBODY.equals(text)) {
+            if (text.length() == 0 || UserACL.NOBODY.equals(text)) {
                 return new String[0];
             } else {
                 List<String> list = new ArrayList<String>(5);
