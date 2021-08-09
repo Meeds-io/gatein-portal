8cabbe443e1def7491dbb6bdb5f2e05ae12c8018
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-08-09 10:40:27 +0100

    TASK-47688 Improve scroll performances

--- web/eXoResources/src/main/webapp/javascript/eXo/portal/BodyScrollListener.js
+++ web/eXoResources/src/main/webapp/javascript/eXo/portal/BodyScrollListener.js
@@ -4,18 +4,32 @@ function installScrollControlListener() {
   const $siteBody = $('#UISiteBody');
   if (!$siteBody.data('scroll-control')) {
     $siteBody.data('scroll-control', 'true');
-    $siteBody.scroll(controlBodyScrollClass);
+    const shadowBox = document.createElement('div');
+    shadowBox.id = 'TopBarBoxShadow';
+    shadowBox.style.boxShadow = '0 6px 4px -4px rgb(0 0 0 / 30%)';
+    shadowBox.style.position = 'fixed';
+    shadowBox.style.width = '100vw';
+    shadowBox.style.height = '56px';
+    shadowBox.style.top = 0;
+    shadowBox.style.left = 0;
+    shadowBox.style.right = 0;
+    shadowBox.style.visibility = 'hidden';
+    document.querySelector('.MiddleToolBarTDContainer .MiddleToolBar').appendChild(shadowBox);
+    $siteBody[0].addEventListener('scroll', controlBodyScrollClass, false);
     controlBodyScrollClass();
   }
 }
 
 function controlBodyScrollClass() {
   const $siteBody = $('#UISiteBody');
+  const topBarBoxShadow = document.querySelector('#TopBarBoxShadow');
   if($siteBody.scrollTop()) {
-    if (!$('#UIWorkingWorkspace').hasClass('body-scrolled')) {
-      $('#UIWorkingWorkspace').addClass('body-scrolled');
+    if (topBarBoxShadow.style.visibility === 'hidden') {
+      document.querySelector('#TopBarBoxShadow').style.visibility = '';
     }
   } else {
-    $('#UIWorkingWorkspace').removeClass('body-scrolled');
+    if (topBarBoxShadow.style.visibility !== 'hidden') {
+      document.querySelector('#TopBarBoxShadow').style.visibility = 'hidden';
+    }
   }
 }
