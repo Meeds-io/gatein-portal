52efa91ae7aeed7954777b903ab0ad29e2de9e5c
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-02-11 13:15:48 +0100

    TASK-54025 Enhance extensibility of Login UI
    Prior to this change, the Login UI wasn't extensible enough to define new buttons and UI components by addons. This change has introduced a new way to extend UI using extensionRegistry and a component plugin that allows to define parameters to retrieve from backend to Login UI for extended UI components.

---
+++ component/web/security/src/main/java/org/exoplatform/web/login/LoginParamsExtension.java
@@ -0,0 +1,38 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ * Copyright (C) 2022 Meeds Association
+ * contact@meeds.io
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.exoplatform.web.login;
+
+import java.util.Map;
+
+import org.exoplatform.web.ControllerContext;
+
+/**
+ * An API for services willing to extend Login UI with additional parameters. To
+ * extend the list of params of Login UI, you can simply implement this
+ * interface and declare it as a Kernel Component.
+ */
+public interface LoginParamsExtension {
+
+  /**
+   * Returns the list of parameters to retrieve in Login UI for Login components
+   * 
+   * @param controllerContext
+   * @return List of Key/Value to add to Login JSON object params
+   */
+  Map<String, Object> extendLoginParameters(ControllerContext controllerContext);
+
+}
