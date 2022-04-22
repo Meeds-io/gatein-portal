2014944a552c1ae8df73ee7e9e2c0d7a52f30fec
Romain Dénarié <romain.denarie@exoplatform.com>
2022-04-22 11:27:10 +0200

    Task-56371 : Unable to login with metamask after few login
    Before this fix, there was a problem with alternative authentication system. After few successul attempts, the account is locked.
    The problem is due to the fact the authencation fail counter is not reset on successful login.
    To fix this problem, the OrganizationAuthenticatorImpl is moved to portal module, and the code to check the authentication attempts is moved from UserDAOImpl to OrganizationAuthenticatorImpl

--- component/common/src/main/java/org/exoplatform/web/login/recovery/ChangePasswordConnector.java
+++
@@ -1,9 +0,0 @@
-package org.exoplatform.web.login.recovery;
-
-import org.exoplatform.container.component.BaseComponentPlugin;
-
-public abstract class ChangePasswordConnector  extends BaseComponentPlugin {
-  
-  public abstract void changePassword(final String username, final String password) throws Exception;
-  protected abstract boolean isAllowChangeExternalPassword();
-}
