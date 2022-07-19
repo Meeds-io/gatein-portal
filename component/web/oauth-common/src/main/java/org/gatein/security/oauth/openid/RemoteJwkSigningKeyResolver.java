6621a87173b7b8537aa596aa01752cd686eb9d55
hchaabene <99255294+hchaabene@users.noreply.github.com>
2022-07-19 11:33:17 +0100

    Task-58777: Validate jwt token for OIDC (#400)
    Prior to this change, tokeninfo url for debugging only, and not in production mode. In production, the token validation should be done locally
    This change , As we know have a key resolver, I update the function validateTokenAndUpdateScopes() with Jwts.parserBuilder()

--- component/web/oauth-common/src/main/java/org/gatein/security/oauth/openid/RemoteJwkSigningKeyResolver.java
+++ component/web/oauth-common/src/main/java/org/gatein/security/oauth/openid/RemoteJwkSigningKeyResolver.java
@@ -21,120 +21,150 @@ import io.jsonwebtoken.SigningKeyResolver;
 import io.jsonwebtoken.io.Decoders;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
-import javax.json.*;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.StringReader;
+import org.json.*;
+
+import java.io.*;
 import java.math.BigInteger;
-import java.net.URL;
-import java.net.URLConnection;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPublicKeySpec;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class RemoteJwkSigningKeyResolver implements SigningKeyResolver {
 
-  private final    String              issuer;
+    private final String issuer;
+    private final Object lock = new Object();
+    private Map<String, Key> keyMap = new HashMap<>();
+    private static final Log LOG = ExoLogger.getLogger(RemoteJwkSigningKeyResolver.class);
 
-
-  private final    Object           lock = new Object();
-
-  private          Map<String, Key> keyMap = new HashMap<>();
-  private static final Log              LOG    = ExoLogger.getLogger(RemoteJwkSigningKeyResolver.class);
-
-
-  public RemoteJwkSigningKeyResolver(String issuer) {
-    this.issuer = issuer;
-  }
-
-  @Override
-  public Key resolveSigningKey(JwsHeader header, Claims claims) {
-    return getKey(header.getKeyId());
-  }
-
-  @Override
-  public Key resolveSigningKey(JwsHeader header, String plaintext) {
-    return getKey(header.getKeyId());
-  }
-
-  private Key getKey(String keyId) {
-
-    // check non synchronized to avoid a lock
-    Key result = keyMap.get(keyId);
-    if (result != null) {
-      return result;
+    RemoteJwkSigningKeyResolver(String issuer) {
+        this.issuer = issuer;
     }
 
-    synchronized (lock) {
-      // once synchronized, check the map once again the a previously
-      // synchronized thread could have already updated they keys
-      result = keyMap.get(keyId);
-      if (result != null) {
-        return result;
-      }
-
-      // finally, fallback to updating the keys, an return a value (or null)
-      updateKeys();
-      return keyMap.get(keyId);
+    @Override
+    public Key resolveSigningKey(JwsHeader header, Claims claims) {
+        return getKey(header.getKeyId());
     }
-  }
 
-  private void updateKeys() {
-
-    JsonObject configuration = getJson(issuer + "/.well-known/openid-configuration");
-    String jwksUrl = configuration.getString("jwks_uri");
-
-    JsonObject keys = getJson(jwksUrl);
-
-    Map<String, Key> newKeys = keys.getJsonArray("keys").stream()
-                                   .map(JsonValue::asJsonObject)
-                                   .filter(json -> "sig".equals(json.getString("use")))
-                                   .filter(json -> "RSA".equals(json.getString("kty")))
-                                   .collect(Collectors.toMap(jsonObject -> jsonObject.getString("kid"), jsonObject -> {
-                                     BigInteger modulus = base64ToBigInteger(jsonObject.getString("n"));
-                                     BigInteger exponent = base64ToBigInteger(jsonObject.getString("e"));
-                                     RSAPublicKeySpec rsaPublicKeySpec = new RSAPublicKeySpec(modulus, exponent);
-                                     try {
-                                       KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-                                       return keyFactory.generatePublic(rsaPublicKeySpec);
-                                     } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-                                       throw new IllegalStateException("Failed to parse public key");
-                                     }
-                                   }));
-
-    keyMap = Collections.unmodifiableMap(newKeys);
-
-  }
-
-  private JsonObject getJson(String url) {
-    StringBuilder content = new StringBuilder();
-    try {
-      URL wellKnownUrl = new URL(url); // creating a url object
-      URLConnection urlConnection = wellKnownUrl.openConnection(); // creating a urlconnection object
-      // wrapping the urlconnection in a bufferedreader
-      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-      String line;
-      // reading from the urlconnection using the bufferedreader
-      while ((line = bufferedReader.readLine()) != null) {
-        content.append(line + "\n");
-      }
-      bufferedReader.close();
-
-    } catch (Exception e) {
-      LOG.error("Error when reading url {}",url);
+    @Override
+    public Key resolveSigningKey(JwsHeader header, String plaintext) {
+        return getKey(header.getKeyId());
     }
-    try (JsonReader reader = Json.createReader(new StringReader(content.toString()))) {
-      return reader.readObject();
+
+    private Key getKey(String keyId) {
+        // check non synchronized to avoid a lock
+        Key result = keyMap.get(keyId);
+        if (result != null) {
+            return result;
+        }
+
+        synchronized (lock) {
+            // once synchronized, check the map once again the a previously
+            // synchronized thread could have already updated they keys
+            result = keyMap.get(keyId);
+            if (result != null) {
+                return result;
+            }
+            // finally, fallback to updating the keys, an return a value (or null)
+            updateKeys();
+            return keyMap.get(keyId);
+        }
     }
-  }
-  private BigInteger base64ToBigInteger(String value) {
-    return new BigInteger(1, Decoders.BASE64URL.decode(value));
-  }
+
+    private void updateKeys() {
+
+        JSONObject configuration = getJson(issuer + "/.well-known/openid-configuration");
+        try {
+            String jwksUrl = configuration != null ? configuration.getString("jwks_uri") : null;
+            JSONObject keys = getJson(jwksUrl);
+            JSONArray arraylist = keys != null ? keys.getJSONArray("keys") : null;
+            Map<String, Key> newKeys = new HashMap<>();
+            if (arraylist != null) {
+                for (int i = 0; i < arraylist.length(); i++) {
+                    JSONObject jsonobjects = arraylist.getJSONObject(i);
+                    if (("sig".equals(jsonobjects.get("use"))) && ("RSA".equals(jsonobjects.get("kty")))) {
+                        BigInteger modulus = base64ToBigInteger(jsonobjects.getString("n"));
+                        BigInteger exponent = base64ToBigInteger(jsonobjects.getString("e"));
+                        RSAPublicKeySpec rsaPublicKeySpec = new RSAPublicKeySpec(modulus, exponent);
+                        PublicKey publicKey = null;
+                        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+                        publicKey = keyFactory.generatePublic(rsaPublicKeySpec);
+                        newKeys.put(jsonobjects.getString("kid"), publicKey);
+                    }
+                }
+                keyMap = Collections.unmodifiableMap(newKeys);
+            }
+        } catch (JSONException e) {
+            LOG.error("can't get keys in JSONObject");
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new IllegalStateException("Failed to parse public key");
+        }
+    }
+
+    public static Map<String, Object> toMap(JSONObject jsonobj) throws JSONException {
+        Map<String, Object> map = new HashMap<>();
+        Iterator<String> keys = jsonobj.keys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            Object value = jsonobj.get(key);
+            if (value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            } else if (value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            map.put(key, value);
+        }
+        return map;
+    }
+
+    public static List<Object> toList(JSONArray array) throws JSONException {
+        List<Object> list = new ArrayList<>();
+        for (int i = 0; i < array.length(); i++) {
+            Object value = array.get(i);
+            if (value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            } else if (value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            list.add(value);
+        }
+        return list;
+    }
+
+    private JSONObject getJson(String url) {
+        try (InputStream input = new URL(url).openStream()) {
+            // Input Stream Object To Start Streaming.
+            BufferedReader re = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+            // Buffer Reading In UTF-8
+            String text = read(re);         // Handy Method To Read Data From BufferReader
+            return new JSONObject(text); // Returning JSON
+        } catch (Exception e) {
+            LOG.error("Error when Read Data From BufferReader" + e.getMessage());
+            return null;
+        }
+    }
+
+    public String read(Reader re) throws IOException {     // class Declaration
+        StringBuilder str = new StringBuilder();     // To Store Url Data In String.
+        int temp;
+        do {
+            temp = re.read();       //reading Charcter By Chracter.
+            str.append((char) temp);
+
+        } while (temp != -1);
+        //  re.read() return -1 when there is end of buffer , data or end of file.
+
+        return str.toString();
+
+    }
+
+    private BigInteger base64ToBigInteger(String value) {
+        return new BigInteger(1, Decoders.BASE64URL.decode(value));
+    }
 }
