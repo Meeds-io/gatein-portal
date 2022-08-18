e836c0cee0d42d6118be35c83796e71dfd79855c
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2022-08-18 14:17:56 +0100

    TASK-59166: Upgrade to openapi swagger 3 (#415)
    TASK-59166: Upgrade to openapi swagger 3 (#411)
    
    Upgrade to openapi swagger 3

--- component/portal/src/main/java/org/exoplatform/portal/branding/BrandingRestResourcesV1.java
+++ component/portal/src/main/java/org/exoplatform/portal/branding/BrandingRestResourcesV1.java
@@ -23,16 +23,20 @@ import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.commons.lang3.StringUtils;
 
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.services.rest.resource.ResourceContainer;
 
-import io.swagger.annotations.*;
 
 @Path("/v1/platform/branding")
-@Api(tags = "/v1/platform/branding", value = "/v1/platform/branding", description = "Managing branding information")
+@Tag(name = "/v1/platform/branding", description = "Managing branding information")
 public class BrandingRestResourcesV1 implements ResourceContainer {
   private static final Log LOG = ExoLogger.getLogger(BrandingRestResourcesV1.class);
 
@@ -52,12 +56,13 @@ public class BrandingRestResourcesV1 implements ResourceContainer {
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @RolesAllowed("users")
-  @ApiOperation(value = "Get Branding information",
-  httpMethod = "GET",
-  response = Response.class)
+  @Operation(
+          summary = "Get Branding information",
+          description = "Get Branding information",
+          method = "GET" )
   @ApiResponses(value = {
-      @ApiResponse (code = 200, message = "Request fulfilled"),
-      @ApiResponse (code = 500, message = "Server error when retrieving branding information") })
+      @ApiResponse(responseCode = "200", description = "Request fulfilled"),
+      @ApiResponse (responseCode = "500", description = "Server error when retrieving branding information") })
   public Response getBrandingInformation() {
     try {
       return Response.ok(brandingService.getBrandingInformation()).build();
@@ -70,10 +75,13 @@ public class BrandingRestResourcesV1 implements ResourceContainer {
   @PUT
   @RolesAllowed("administrators")
   @Consumes(MediaType.APPLICATION_JSON)
-  @ApiOperation(value = "Update Branding information", httpMethod = "POST", response = Response.class)
+  @Operation(
+          summary = "Update Branding information", 
+          description = "Update Branding information",
+          method = "POST")
   @ApiResponses(value = {
-      @ApiResponse(code = 200, message = "Branding information updated"),
-      @ApiResponse(code = 500, message = "Server error when updating branding information") })
+      @ApiResponse(responseCode = "200", description = "Branding information updated"),
+      @ApiResponse(responseCode = "500", description = "Server error when updating branding information") })
   public Response updateBrandingInformation(Branding branding) {
     try {
       brandingService.updateBrandingInformation(branding);
@@ -87,13 +95,17 @@ public class BrandingRestResourcesV1 implements ResourceContainer {
   @GET
   @Path("/logo")
   @Produces("image/png")
-  @ApiOperation(value = "Get Branding logo", httpMethod = "GET", response = Response.class)
+  @Operation(
+          summary = "Get Branding logo", 
+          description = "Get Branding logo",
+          method = "GET")
   @ApiResponses(value = {
-          @ApiResponse(code = 200, message = "Branding logo retrieved"),
-          @ApiResponse(code = 404, message = "Branding logo not found"),
-          @ApiResponse(code = 500, message = "Server error when retrieving branding logo") })
+          @ApiResponse(responseCode = "200", description = "Branding logo retrieved"),
+          @ApiResponse(responseCode = "404", description = "Branding logo not found"),
+          @ApiResponse(responseCode = "500", description = "Server error when retrieving branding logo") })
   public Response getBrandingLogo(@Context Request request,
-                                  @ApiParam(value = "The value of lastModified parameter will determine whether the query should be cached by browser or not. If not set, no 'expires HTTP Header will be sent'") @QueryParam("lastModified") String lastModified) {
+                                  @Parameter(description = "The value of lastModified parameter will determine whether the query should be cached by browser or not. If not set, no 'expires HTTP Header will be sent'")
+                                  @QueryParam("lastModified") String lastModified) {
 
     Logo logo = brandingService.getLogo();
     if (logo == null) {
@@ -129,14 +141,18 @@ public class BrandingRestResourcesV1 implements ResourceContainer {
 
   @GET
   @Path("/css")
-  @ApiOperation(value = "Get Branding CSS content", httpMethod = "GET", response = Response.class)
+  @Operation(
+          summary = "Get Branding CSS content", 
+          description = "Get Branding CSS content",
+          method = "GET")
   @ApiResponses(value = {
-      @ApiResponse(code = 200, message = "Branding css retrieved"),
-      @ApiResponse(code = 304, message = "Branding css not modified"),
-      @ApiResponse(code = 500, message = "Server error when retrieving branding css")
+      @ApiResponse(responseCode = "200", description = "Branding css retrieved"),
+      @ApiResponse(responseCode = "304", description = "Branding css not modified"),
+      @ApiResponse(responseCode = "500", description = "Server error when retrieving branding css")
   })
   public Response getBrandingCSS(@Context Request request,
-                                 @ApiParam(value = "The value of lastModified parameter will determine whether the query should be cached by browser or not. If not set, no 'expires HTTP Header will be sent'") @QueryParam("v") String lastModified) {
+                                 @Parameter(description = "The value of lastModified parameter will determine whether the query should be cached by browser or not. If not set, no 'expires HTTP Header will be sent'")
+                                 @QueryParam("v") String lastModified) {
     //
     long lastUpdated = brandingService.getLastUpdatedTime();
     EntityTag eTag = new EntityTag(String.valueOf(lastUpdated));
@@ -167,11 +183,14 @@ public class BrandingRestResourcesV1 implements ResourceContainer {
   @GET
   @Path("/defaultLogo")
   @RolesAllowed("users")
-  @ApiOperation(value = "Get Branding default logo", httpMethod = "GET", response = Response.class)
+  @Operation(
+          summary = "Get Branding default logo",
+          description = "Get Branding default logo",
+          method = "GET")
   @ApiResponses(value = {
-          @ApiResponse(code = 200, message = "Branding default logo retrieved"),
-          @ApiResponse(code = 404, message = "Branding default logo not found"),
-          @ApiResponse(code = 500, message = "Server error when retrieving branding default logo") })
+          @ApiResponse(responseCode = "200", description = "Branding default logo retrieved"),
+          @ApiResponse(responseCode = "404", description = "Branding default logo not found"),
+          @ApiResponse(responseCode = "500", description = "Server error when retrieving branding default logo") })
   public Response getBrandingDefaultLogo(@Context Request request) {
     Logo logo = brandingService.getDefaultLogo();
     if (logo == null) {
