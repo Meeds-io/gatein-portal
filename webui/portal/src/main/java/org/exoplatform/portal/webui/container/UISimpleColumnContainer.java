15c8c0a93300aa7d0090c3679d674111237bf43b
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2023-01-06 10:47:16 +0100

    feat: Fix insert app permission for a new inserted simple column container - EXO-61650 - Meeds/io-MIPs#37 (#506)

--- webui/portal/src/main/java/org/exoplatform/portal/webui/container/UISimpleColumnContainer.java
+++ webui/portal/src/main/java/org/exoplatform/portal/webui/container/UISimpleColumnContainer.java
@@ -14,55 +14,56 @@ import org.exoplatform.webui.event.Event;
 import org.exoplatform.webui.event.EventListener;
 
 @ComponentConfig(template = "system:/groovy/portal/webui/container/UISimpleColumnContainer.gtmpl", events = {
-        @EventConfig(listeners = UISimpleColumnContainer.InsertColumnActionListener.class),
-        @EventConfig(listeners = DeleteComponentActionListener.class, confirm = "UIColumnContainer.deleteColumnContainer"),
-        @EventConfig(listeners = EditContainerActionListener.class) })
+    @EventConfig(listeners = UISimpleColumnContainer.InsertColumnActionListener.class),
+    @EventConfig(listeners = DeleteComponentActionListener.class, confirm = "UIColumnContainer.deleteColumnContainer"),
+    @EventConfig(listeners = EditContainerActionListener.class) })
 public class UISimpleColumnContainer extends UIContainer {
-    public static final String SIMPLE_COLUMN_CONTAINER = "SimpleColumnContainer";
+  public static final String SIMPLE_COLUMN_CONTAINER = "SimpleColumnContainer";
 
-    public static final String INSERT_AFTER = "insertColumnAfter";
+  public static final String INSERT_AFTER            = "insertColumnAfter";
 
-    public static final String INSERT_BEFORE = "insertColumnBefore";
+  public static final String INSERT_BEFORE           = "insertColumnBefore";
 
-    public UISimpleColumnContainer() {
-        super();
+  public UISimpleColumnContainer() {
+    super();
+  }
+
+  public static class InsertColumnActionListener extends EventListener<UISimpleColumnContainer> {
+    @Override
+    public void execute(Event<UISimpleColumnContainer> event) throws Exception {
+      String insertPosition = event.getRequestContext().getRequestParameter(UIComponent.OBJECTID);
+      UISimpleColumnContainer uiSelectedColumn = event.getSource();
+      UIPortalComponent uiParent = (UIPortalComponent) uiSelectedColumn.getParent();
+      if (insertPosition.equals(INSERT_AFTER)) {
+        UISimpleColumnContainer.insertColumn(uiSelectedColumn, true);
+      } else if (insertPosition.equals(INSERT_BEFORE)) {
+        UISimpleColumnContainer.insertColumn(uiSelectedColumn, false);
+      }
+
+      PortalRequestContext pcontext = (PortalRequestContext) event.getRequestContext();
+      pcontext.addUIComponentToUpdateByAjax(uiParent);
+      pcontext.ignoreAJAXUpdateOnPortlets(true);
+      pcontext.getJavascriptManager()
+              .require("SHARED/portalComposer", "portalComposer")
+              .addScripts("portalComposer.toggleSaveButton();");
     }
+  }
 
-    public static class InsertColumnActionListener extends EventListener<UISimpleColumnContainer> {
-        @Override
-        public void execute(Event<UISimpleColumnContainer> event) throws Exception {
-            String insertPosition = event.getRequestContext().getRequestParameter(UIComponent.OBJECTID);
-            UISimpleColumnContainer uiSelectedColumn = event.getSource();
-            UIPortalComponent uiParent = (UIPortalComponent) uiSelectedColumn.getParent();
-            if (insertPosition.equals(INSERT_AFTER)) {
-                UISimpleColumnContainer.insertColumn(uiSelectedColumn, true);
-            } else if (insertPosition.equals(INSERT_BEFORE)) {
-                UISimpleColumnContainer.insertColumn(uiSelectedColumn, false);
-            }
+  private static void insertColumn(UISimpleColumnContainer selectedColumn, boolean isInsertAfter) throws Exception {
+    UIContainer uiParent = selectedColumn.getParent();
+    UISimpleColumnContainer uiNewColumn = uiParent.addChild(UISimpleColumnContainer.class, null, null);
 
-            PortalRequestContext pcontext = (PortalRequestContext) event.getRequestContext();
-            pcontext.addUIComponentToUpdateByAjax(uiParent);
-            pcontext.ignoreAJAXUpdateOnPortlets(true);
-            pcontext.getJavascriptManager().require("SHARED/portalComposer", "portalComposer")
-                    .addScripts("portalComposer.toggleSaveButton();");
-        }
-
+    uiNewColumn.setTemplate(selectedColumn.getTemplate());
+    uiNewColumn.setFactoryId(selectedColumn.getFactoryId());
+    uiNewColumn.setId(String.valueOf(Math.abs(uiNewColumn.hashCode())));
+    uiNewColumn.setMoveAppsPermissions(uiNewColumn.getAccessPermissions());
+    uiNewColumn.setMoveContainersPermissions(uiNewColumn.getAccessPermissions());;
+    List<UIComponent> listColumn = uiParent.getChildren();
+    int position = listColumn.indexOf(selectedColumn);
+    if (isInsertAfter) {
+      position += 1;
     }
-
-    private static void insertColumn(UISimpleColumnContainer selectedColumn, boolean isInsertAfter) throws Exception {
-        UIContainer uiParent = selectedColumn.getParent();
-        UISimpleColumnContainer uiNewColumn = uiParent.addChild(UISimpleColumnContainer.class, null, null);
-
-        uiNewColumn.setTemplate(selectedColumn.getTemplate());
-        uiNewColumn.setFactoryId(selectedColumn.getFactoryId());
-        uiNewColumn.setId(String.valueOf(Math.abs(uiNewColumn.hashCode())));
-
-        List<UIComponent> listColumn = uiParent.getChildren();
-        int position = listColumn.indexOf(selectedColumn);
-        if (isInsertAfter) {
-            position += 1;
-        }
-        Collections.rotate(listColumn.subList(position, listColumn.size()), 1);
-    }
+    Collections.rotate(listColumn.subList(position, listColumn.size()), 1);
+  }
 
 }
