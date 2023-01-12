1975c730c2a5e98e974d60f78534fc93406bc445
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2023-01-12 23:51:55 +0100

    feat: Fix meeds header - EXO-61137 - EXO-61141 - EXO-61142 - EXO-61480 - Meeds-io/MPIs#37

--- webui/portal/src/main/java/org/exoplatform/portal/webui/container/UISimpleColumnContainer.java
+++ webui/portal/src/main/java/org/exoplatform/portal/webui/container/UISimpleColumnContainer.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ * 
+ * Copyright (C) 2023 Meeds Association contact@meeds.io
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.exoplatform.portal.webui.container;
 
 import java.util.Collections;
@@ -57,7 +76,7 @@ public class UISimpleColumnContainer extends UIContainer {
     uiNewColumn.setFactoryId(selectedColumn.getFactoryId());
     uiNewColumn.setId(String.valueOf(Math.abs(uiNewColumn.hashCode())));
     uiNewColumn.setMoveAppsPermissions(uiNewColumn.getAccessPermissions());
-    uiNewColumn.setMoveContainersPermissions(uiNewColumn.getAccessPermissions());;
+    uiNewColumn.setMoveContainersPermissions(uiNewColumn.getAccessPermissions());
     List<UIComponent> listColumn = uiParent.getChildren();
     int position = listColumn.indexOf(selectedColumn);
     if (isInsertAfter) {
