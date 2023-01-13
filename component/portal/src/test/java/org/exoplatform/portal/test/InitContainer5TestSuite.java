b8cfca9a38e4c4e8306063c86a07b93afbf40943
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-01-13 16:53:04 +0100

    feat: Add Favicon in Branding API to allow customize it through UI - MEED-1499 - Meeds-io/MIPs#33 (#519)
    This change will introduce a new API endpoint for branding to update and
    retrieve favicon.

--- component/portal/src/test/java/org/exoplatform/portal/test/InitContainer5TestSuite.java
+++ component/portal/src/test/java/org/exoplatform/portal/test/InitContainer5TestSuite.java
@@ -1,8 +1,6 @@
 /*
  * This file is part of the Meeds project (https://meeds.io/).
- *
  * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
- *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -15,40 +13,18 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-package org.exoplatform.portal.branding;
+package org.exoplatform.portal.test;
 
-import java.io.Serializable;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
-import javax.xml.bind.annotation.XmlRootElement;
+import org.exoplatform.portal.mop.jdbc.service.TestDataStorage;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@XmlRootElement
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Logo implements Serializable {
-
-  private static final long serialVersionUID = 5444110675143558828L;
-
-  @Getter
-  @Setter
-  private String uploadId;
-
-  @Getter
-  @Setter
-  private long   size;
-
-  @Getter
-  @Setter
-  private byte[] data;
-
-  @Getter
-  @Setter
-  private long updatedDate;
+@RunWith(Suite.class)
+@SuiteClasses({
+  TestDataStorage.class,
+})
+public class InitContainer5TestSuite {
 
 }
