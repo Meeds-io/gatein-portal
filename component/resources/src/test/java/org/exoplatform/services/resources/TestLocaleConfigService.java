8194b1daa477c374b38830f5dca043666a5857ee
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-06-15 14:43:56 +0100

    feat: Allow to store Global Default Language in DB for LocaleConfiguService - MEED-2260 - Meeds-io/meeds#994 (#614)
    Prior to this change, the default language was set using an extension
    configuration from locale-config.xml. In fact, the first language
    configuration was used as default language. This change will unify the
    definition of default configuration using LocaleConfigService and will
    allow to change this default config by allowing to persist a new
    preferred value.

--- component/resources/src/test/java/org/exoplatform/services/resources/TestLocaleConfigService.java
+++ component/resources/src/test/java/org/exoplatform/services/resources/TestLocaleConfigService.java
@@ -19,103 +19,194 @@
 
 package org.exoplatform.services.resources;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
+
+import org.exoplatform.commons.api.settings.SettingService;
 import org.exoplatform.commons.utils.PropertyManager;
 import org.exoplatform.container.configuration.ConfigurationManagerImpl;
 import org.exoplatform.container.xml.InitParams;
 import org.exoplatform.container.xml.ValueParam;
+import org.exoplatform.services.listener.ListenerService;
 import org.exoplatform.services.resources.impl.LocaleConfigImpl;
 import org.exoplatform.services.resources.impl.LocaleConfigServiceImpl;
 
 /**
- * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @author  <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
  * @version $Revision$
  */
-public class TestLocaleConfigService extends AbstractResourceBundleTest {
+public class TestLocaleConfigService extends AbstractResourceBundleTest { // NOSONAR
 
-    public TestLocaleConfigService() {
+  private ListenerService listenerService;
+
+  public void testParseLocaleConfigFile() throws Exception {
+    PropertyManager.setProperty(PropertyManager.DEVELOPING, "false");
+    LocaleConfigService service = createService();
+    Map<String, LocaleConfig> map = createMap(service);
+    assertEquals(4, map.size());
+    assertCommonConfigs(service);
+  }
+
+  public void testDevMode() throws Exception {
+    PropertyManager.setProperty(PropertyManager.DEVELOPING, "true");
+    LocaleConfigService service = createService();
+    Map<String, LocaleConfig> map = createMap(service);
+    assertEquals(5, map.size());
+    assertCommonConfigs(service);
+    LocaleConfig ma = service.getLocaleConfig("ma");
+    assertLocaleConfig(ma,
+                       "ma",
+                       "Default configuration for the debugging locale",
+                       "UTF-8",
+                       "UTF-8",
+                       Orientation.LT,
+                       IdentityResourceBundle.MAGIC_LOCALE);
+  }
+
+  public void testDefaultConfiguredLanguage() throws Exception {
+    LocaleConfigService service = createService("fr");
+    LocaleConfig defaultLocaleConfig = service.getDefaultLocaleConfig();
+    assertNotNull(defaultLocaleConfig);
+    assertEquals("fr", defaultLocaleConfig.getLocaleName());
+  }
+
+  public void testDefaultStoredLanguage() throws Exception {
+    LocaleConfigService service = createService("fr");
+    service.saveDefaultLocaleConfig("en");
+
+    verify(listenerService, times(1)).broadcast(LocaleConfigServiceImpl.LOCALE_DEFAULT_MODIFIED_EVENT, "fr", "en");
+
+    LocaleConfig defaultLocaleConfig = service.getDefaultLocaleConfig();
+    assertNotNull(defaultLocaleConfig);
+    assertEquals("en", defaultLocaleConfig.getLocaleName());
+  }
+
+  public void testRemoveDefaultStoredLanguage() throws Exception {
+    LocaleConfigService service = createService("fr");
+    service.saveDefaultLocaleConfig("en");
+    verify(listenerService, times(1)).broadcast(LocaleConfigServiceImpl.LOCALE_DEFAULT_MODIFIED_EVENT, "fr", "en");
+    service.saveDefaultLocaleConfig(null);
+    verify(listenerService, times(1)).broadcast(LocaleConfigServiceImpl.LOCALE_DEFAULT_MODIFIED_EVENT, "en", "fr");
+
+    LocaleConfig defaultLocaleConfig = service.getDefaultLocaleConfig();
+    assertNotNull(defaultLocaleConfig);
+    assertEquals("fr", defaultLocaleConfig.getLocaleName());
+  }
+
+  public void testLocaleConfig() {
+    LocaleConfig ma = new LocaleConfigImpl();
+    ma.setLocale("ma");
+    assertEquals("ma", ma.getLocaleName());
+
+    LocaleConfig ma1 = new LocaleConfigImpl();
+    ma1.setLocale(new Locale("ma"));
+    assertEquals("ma", ma1.getLocaleName());
+  }
+
+  private Map<String, LocaleConfig> createMap(LocaleConfigService service) {
+    Map<String, LocaleConfig> map = new HashMap<>();
+    for (LocaleConfig config : service.getLocalConfigs()) {
+      map.put(config.getLanguage(), config);
+    }
+    return map;
+  }
+
+  private LocaleConfigService createService() throws Exception {
+    return createService(null);
+  }
+
+  private LocaleConfigService createService(String defaultLang) throws Exception {
+    ConfigurationManagerImpl cm = new ConfigurationManagerImpl();
+    InitParams params = new InitParams();
+    ValueParam param = new ValueParam();
+    param.setName("locale.config.file");
+    param.setValue("classpath:/resources/locales-config.xml");
+    params.addParameter(param);
+
+    if (StringUtils.isNotBlank(defaultLang)) {
+      param = new ValueParam();
+      param.setName("locale.config.default");
+      param.setValue(defaultLang);
+      params.addParameter(param);
     }
 
-    public TestLocaleConfigService(String s) {
-        super(s);
-    }
+    SettingService settingService = mock(SettingService.class);
+    this.listenerService = mock(ListenerService.class);
+    doAnswer(invocation -> {
+      when(settingService.get(invocation.getArgument(0),
+                              invocation.getArgument(1),
+                              invocation.getArgument(2))).thenReturn(invocation.getArgument(3));
+      return null;
+    }).when(settingService)
+      .set(any(),
+           any(),
+           any(),
+           any());
+    doAnswer(invocation -> {
+      reset(settingService);
+      return null;
+    }).when(settingService)
+      .remove(any(),
+              any(),
+              any());
 
-    public void testFoo() throws Exception {
-        PropertyManager.setProperty(PropertyManager.DEVELOPING, "false");
-        LocaleConfigService service = createService();
-        Map<String, LocaleConfig> map = createMap(service);
-        assertEquals(4, map.size());
-        assertCommonConfigs(service);
-    }
+    return new LocaleConfigServiceImpl(settingService, listenerService, cm, params);
+  }
 
-    public void testBar() throws Exception {
-        PropertyManager.setProperty(PropertyManager.DEVELOPING, "true");
-        LocaleConfigService service = createService();
-        Map<String, LocaleConfig> map = createMap(service);
-        assertEquals(5, map.size());
-        assertCommonConfigs(service);
-        LocaleConfig ma = service.getLocaleConfig("ma");
-        assertLocaleConfig(ma, "ma", "Default configuration for the debugging locale", "UTF-8", "UTF-8", Orientation.LT,
-                IdentityResourceBundle.MAGIC_LOCALE);
-    }
+  private void assertCommonConfigs(LocaleConfigService service) {
+    LocaleConfig en = service.getLocaleConfig("en");
+    LocaleConfig fr = service.getLocaleConfig("fr");
+    LocaleConfig ar = service.getLocaleConfig("ar");
+    LocaleConfig vi = service.getLocaleConfig("vi");
+    assertLocaleConfig(en,
+                       "en",
+                       "Default configuration for english locale",
+                       "UTF-8",
+                       "UTF-8",
+                       Orientation.LT,
+                       Locale.ENGLISH);
+    assertLocaleConfig(fr,
+                       "fr",
+                       "Default configuration for the french locale",
+                       "UTF-8",
+                       "UTF-8",
+                       Orientation.LT,
+                       Locale.FRENCH);
+    assertLocaleConfig(ar,
+                       "ar",
+                       "Default configuration for the arabic locale",
+                       "UTF-8",
+                       "UTF-8",
+                       Orientation.RT,
+                       new Locale("ar"));
+    assertLocaleConfig(vi,
+                       "vi",
+                       "Default configuration for the vietnam locale",
+                       "UTF-8",
+                       "UTF-8",
+                       Orientation.LT,
+                       new Locale("vi"));
+  }
 
-    public void testLocaleConfig() throws Exception {
-        LocaleConfig ma = new LocaleConfigImpl();
-        ma.setLocale("ma");
-        assertEquals("ma", ma.getLocaleName());
-
-        LocaleConfig ma1 = new LocaleConfigImpl();
-        ma1.setLocale(new Locale("ma"));
-        assertEquals("ma", ma1.getLocaleName());
-    }
-
-    private Map<String, LocaleConfig> createMap(LocaleConfigService service) {
-        Map<String, LocaleConfig> map = new HashMap<String, LocaleConfig>();
-        for (LocaleConfig config : service.getLocalConfigs()) {
-            map.put(config.getLanguage(), config);
-        }
-        return map;
-    }
-
-    private LocaleConfigService createService() throws Exception {
-        ConfigurationManagerImpl cm = new ConfigurationManagerImpl();
-        InitParams params = new InitParams();
-        ValueParam param = new ValueParam();
-        param.setName("locale.config.file");
-        param.setValue("classpath:/resources/locales-config.xml");
-        params.addParameter(param);
-
-        //
-        LocaleConfigService service = new LocaleConfigServiceImpl(params, cm);
-        return service;
-    }
-
-    private void assertCommonConfigs(LocaleConfigService service) {
-        LocaleConfig en = service.getLocaleConfig("en");
-        LocaleConfig fr = service.getLocaleConfig("fr");
-        LocaleConfig ar = service.getLocaleConfig("ar");
-        LocaleConfig vi = service.getLocaleConfig("vi");
-        assertLocaleConfig(en, "en", "Default configuration for english locale", "UTF-8", "UTF-8", Orientation.LT,
-                Locale.ENGLISH);
-        assertLocaleConfig(fr, "fr", "Default configuration for the french locale", "UTF-8", "UTF-8", Orientation.LT,
-                Locale.FRENCH);
-        assertLocaleConfig(ar, "ar", "Default configuration for the arabic locale", "UTF-8", "UTF-8", Orientation.RT,
-                new Locale("ar"));
-        assertLocaleConfig(vi, "vi", "Default configuration for the vietnam locale", "UTF-8", "UTF-8", Orientation.LT,
-                new Locale("vi"));
-    }
-
-    private void assertLocaleConfig(LocaleConfig config, String language, String description, String inputEncoding,
-            String outputEncoding, Orientation orientation, Locale locale) {
-        assertNotNull(config);
-        assertEquals(language, config.getLanguage());
-        assertEquals(description, config.getDescription());
-        assertEquals(inputEncoding, config.getInputEncoding());
-        assertEquals(outputEncoding, config.getOutputEncoding());
-        assertEquals(orientation, config.getOrientation());
-        assertEquals(locale, config.getLocale());
-    }
+  private void assertLocaleConfig(LocaleConfig config, String language, String description, String inputEncoding,
+                                  String outputEncoding, Orientation orientation, Locale locale) {
+    assertNotNull(config);
+    assertEquals(language, config.getLanguage());
+    assertEquals(description, config.getDescription());
+    assertEquals(inputEncoding, config.getInputEncoding());
+    assertEquals(outputEncoding, config.getOutputEncoding());
+    assertEquals(orientation, config.getOrientation());
+    assertEquals(locale, config.getLocale());
+  }
 }
