f9b106bf96189a50d31d3a9cb12bed840666de34
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2023-08-30 19:50:38 +0100

    feat: Implement getSites rest endpoint and services in order to retrieve sites with navigations - EXO-65477 - Meeds-io/MIPs#72 (#652)

--- component/portal/src/main/java/org/exoplatform/portal/mop/dao/SiteDAOImpl.java
+++ component/portal/src/main/java/org/exoplatform/portal/mop/dao/SiteDAOImpl.java
@@ -1,27 +1,39 @@
 package org.exoplatform.portal.mop.dao;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 
+import org.apache.commons.lang.StringUtils;
+
 import org.exoplatform.commons.api.persistence.ExoTransactional;
 import org.exoplatform.portal.jdbc.entity.SiteEntity;
+import org.exoplatform.portal.mop.SiteFilter;
 import org.exoplatform.portal.mop.SiteKey;
 import org.exoplatform.portal.mop.SiteType;
 
 public class SiteDAOImpl extends AbstractDAO<SiteEntity> implements SiteDAO {
 
-  private static final String NAME                   = "name";
+  private static final String        SITE_NAME                = "name";
 
-  private static final String SITE_TYPE              = "siteType";
+  private static final String        SITE_TYPE                = "siteType";
 
-  private static final String SPACE_SITE_TYPE_PREFIX = "/spaces/";
+  private static final String        DISPLAYED                = "displayed";
 
-  private PageDAO             pageDAO;
+  private static final String        SPACE_SITE_TYPE_PREFIX   = "/spaces/";
 
-  private NavigationDAO       navDAO;
+  private static final String        QUERY_FILTER_FIND_PREFIX = "SiteEntity.findSites";
+
+  private final Map<String, Boolean> filterNamedQueries       = new HashMap<>();
+
+  private PageDAO                    pageDAO;
+
+  private NavigationDAO              navDAO;
 
   public SiteDAOImpl(PageDAO pageDAO, NavigationDAO navDAO) {
     super();
@@ -56,7 +68,7 @@ public class SiteDAOImpl extends AbstractDAO<SiteEntity> implements SiteDAO {
     TypedQuery<SiteEntity> query = getEntityManager().createNamedQuery("SiteEntity.findByKey", SiteEntity.class);
 
     query.setParameter(SITE_TYPE, siteKey.getType());
-    query.setParameter(NAME, siteKey.getName());
+    query.setParameter(SITE_NAME, siteKey.getName());
     try {
       return query.getSingleResult();
     } catch (NoResultException ex) {
@@ -123,6 +135,19 @@ public class SiteDAOImpl extends AbstractDAO<SiteEntity> implements SiteDAO {
     return getSiteNames(SiteType.USER, offset, limit);
   }
 
+  @Override
+  public List<SiteEntity> findSites(SiteFilter siteFilter) {
+    TypedQuery<SiteEntity> query = buildQueryFromFilter(siteFilter);
+    if (siteFilter.getOffset() > 0) {
+      query.setFirstResult(siteFilter.getOffset());
+    }
+    if (siteFilter.getLimit() > 0) {
+      query.setMaxResults(siteFilter.getLimit());
+    }
+    List<SiteEntity> resultList = query.getResultList();
+    return resultList == null ? Collections.emptyList() : resultList;
+  }
+
   private List<String> getSiteNames(SiteType siteType, int offset, int limit) {
     TypedQuery<String> query = getEntityManager().createNamedQuery("SiteEntity.findPortalSites", String.class);
     query.setParameter(SITE_TYPE, siteType);
@@ -135,4 +160,67 @@ public class SiteDAOImpl extends AbstractDAO<SiteEntity> implements SiteDAO {
     return query.getResultList();
   }
 
+  private <T> TypedQuery<T> buildQueryFromFilter(SiteFilter filter) {
+    List<String> suffixes = new ArrayList<>();
+    List<String> predicates = new ArrayList<>();
+    buildPredicates(filter, suffixes, predicates);
+
+    TypedQuery<T> query;
+    String queryName = getQueryFilterName(suffixes);
+    if (filterNamedQueries.containsKey(queryName)) {
+      query = (TypedQuery<T>) getEntityManager().createNamedQuery(queryName, SiteEntity.class);
+    } else {
+      String queryContent = getQueryFilterContent(predicates);
+      query = (TypedQuery<T>) getEntityManager().createQuery(queryContent, SiteEntity.class);
+      getEntityManager().getEntityManagerFactory().addNamedQuery(queryName, query);
+      filterNamedQueries.put(queryName, true);
+    }
+
+    addQueryFilterParameters(filter, query);
+    return query;
+  }
+
+  private void buildPredicates(SiteFilter filter, List<String> suffixes, List<String> predicates) {
+    if (filter.getSiteType() != null) {
+      suffixes.add("Type");
+      predicates.add("s.siteType = :siteType");
+    }
+    if (!filter.isAllSites()) {
+      suffixes.add("Displayed");
+      predicates.add("s.displayed = :displayed");
+    }
+    if (StringUtils.isNotBlank(filter.getExcludedSiteName())) {
+      suffixes.add("Excluding" + filter.getExcludedSiteName());
+      predicates.add("s.name != :name");
+    }
+  }
+
+  private String getQueryFilterName(List<String> suffixes) {
+    return suffixes.isEmpty() ? QUERY_FILTER_FIND_PREFIX : QUERY_FILTER_FIND_PREFIX + "By" + StringUtils.join(suffixes, "And");
+  }
+
+  private <T> void addQueryFilterParameters(SiteFilter filter, TypedQuery<T> query) {
+    if (filter.getSiteType() != null) {
+      query.setParameter(SITE_TYPE, filter.getSiteType());
+    }
+    if (!filter.isAllSites()) {
+      query.setParameter(DISPLAYED, filter.isDisplayed());
+    }
+    if (StringUtils.isNotBlank(filter.getExcludedSiteName())) {
+      query.setParameter(SITE_NAME, filter.getExcludedSiteName());
+    }
+  }
+
+  private String getQueryFilterContent(List<String> predicates) {
+    String querySelect = "SELECT s FROM GateInSite s ";
+
+    String queryContent;
+    if (predicates.isEmpty()) {
+      queryContent = querySelect;
+    } else {
+      queryContent = querySelect + " WHERE " + StringUtils.join(predicates, " AND ");
+    }
+    queryContent +=  " ORDER BY s.displayOrder ASC, s.label ASC";
+    return queryContent;
+  }
 }
--- component/portal/src/test/java/org/exoplatform/portal/mop/dao/SiteDAOTest.java
+++ component/portal/src/test/java/org/exoplatform/portal/mop/dao/SiteDAOTest.java
@@ -3,9 +3,9 @@ package org.exoplatform.portal.mop.dao;
 import java.util.List;
 
 import org.exoplatform.portal.jdbc.entity.SiteEntity;
+import org.exoplatform.portal.mop.SiteFilter;
 import org.exoplatform.portal.mop.SiteKey;
 import org.exoplatform.portal.mop.SiteType;
-import org.exoplatform.portal.mop.dao.SiteDAO;
 
 public class SiteDAOTest extends AbstractDAOTest {
   private SiteDAO siteDAO;
@@ -24,35 +24,73 @@ public class SiteDAOTest extends AbstractDAOTest {
     super.tearDown();
     end();
   }
-  
+
   public void testCreate() {
     SiteEntity siteEntity = creatSiteEntity();
     siteDAO.create(siteEntity);
     restartTransaction();
-    
+
     SiteEntity result = siteDAO.find(siteEntity.getId());
     assertNotNull(result);
     assertSite(siteEntity, result);
 
     siteDAO.delete(siteEntity);
   }
-  
+
   public void testFind() {
     SiteEntity siteEntity = creatSiteEntity();
+    siteEntity.setDisplayed(true);
+    siteEntity.setDisplayOrder(1);
     siteDAO.create(siteEntity);
     restartTransaction();
-    
+
     SiteEntity result = siteDAO.findByKey(new SiteKey(siteEntity.getSiteType(), siteEntity.getName()));
     assertNotNull(result);
     assertSite(siteEntity, result);
-    
+
     List<SiteEntity> results = siteDAO.findByType(siteEntity.getSiteType());
     assertEquals(1, results.size());
-    
+
     List<SiteKey> keys = siteDAO.findSiteKey(siteEntity.getSiteType());
     assertEquals(1, keys.size());
 
+    SiteEntity siteEntity2 = creatSiteEntity();
+    siteEntity2.setName("siteName");
+    siteEntity2.setDisplayed(true);
+    siteEntity2.setDisplayOrder(2);
+    siteDAO.create(siteEntity2);
+    restartTransaction();
+    
+    SiteFilter filter = new SiteFilter();
+    filter.setDisplayed(false);
+    results = siteDAO.findSites(filter);
+    assertEquals(0, results.size());
+
+    filter.setDisplayed(true);
+    filter.setSiteType(SiteType.PORTAL);
+    results = siteDAO.findSites(filter);
+    assertEquals(2, results.size());
+    assertEquals(1, results.get(0).getDisplayOrder());
+    assertEquals(2, results.get(1).getDisplayOrder());
+    assertEquals(siteEntity.getName(), results.get(0).getName());
+    assertEquals(siteEntity2.getName(), results.get(1).getName());
+    
+    filter.setExcludedSiteName("siteName");
+    results = siteDAO.findSites(filter);
+    assertEquals(1, results.size());
+    assertEquals(siteEntity.getName(), results.get(0).getName());
+    filter.setExcludedSiteName(null);
+
+    filter.setSiteType(SiteType.GROUP);
+    results = siteDAO.findSites(filter);
+    assertEquals(0, results.size());
+
+    filter.setSiteType(null);
+    results = siteDAO.findSites(filter);
+    assertEquals(2, results.size());
+    
     siteDAO.delete(siteEntity);
+    siteDAO.delete(siteEntity2);
   }
 
   private void assertSite(SiteEntity expected, SiteEntity siteEntity) {
