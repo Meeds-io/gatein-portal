137d8632eae873b14dd5fb0ed21835bcc24d877b
IlhemEssaadi <93516534+IlhemEssaadi@users.noreply.github.com>
2023-09-04 16:54:41 +0100

    feat: Implement getSites rest endpoint and services in order to retrieve sites with navigations - EXO-65477 - Meeds-io/MIPs#72 (#656)

--- component/portal/src/main/java/org/exoplatform/portal/mop/rest/EntityBuilder.java
+++ component/portal/src/main/java/org/exoplatform/portal/mop/rest/EntityBuilder.java
@@ -23,53 +23,24 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
 
-import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.portal.config.UserACL;
-import org.exoplatform.portal.config.UserPortalConfig;
-import org.exoplatform.portal.config.UserPortalConfigService;
 import org.exoplatform.portal.config.model.Page;
-import org.exoplatform.portal.config.model.PortalConfig;
 import org.exoplatform.portal.mop.PageType;
-import org.exoplatform.portal.mop.SiteFilter;
-import org.exoplatform.portal.mop.SiteKey;
-import org.exoplatform.portal.mop.SiteType;
-import org.exoplatform.portal.mop.navigation.Scope;
-import org.exoplatform.portal.mop.rest.model.SiteRestEntity;
 import org.exoplatform.portal.mop.rest.model.UserNodeRestEntity;
 import org.exoplatform.portal.mop.service.LayoutService;
-import org.exoplatform.portal.mop.user.HttpUserPortalContext;
-import org.exoplatform.portal.mop.user.UserNavigation;
 import org.exoplatform.portal.mop.user.UserNode;
-import org.exoplatform.portal.mop.user.UserNodeFilterConfig;
-import org.exoplatform.portal.mop.user.UserPortal;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
-import org.exoplatform.services.organization.Group;
 import org.exoplatform.services.organization.OrganizationService;
-import org.exoplatform.services.security.ConversationState;
-import org.exoplatform.services.security.Identity;
 
 public class EntityBuilder {
-
-  private static final Log               LOG   = ExoLogger.getLogger(EntityBuilder.class);
+  private static final Log LOG = ExoLogger.getLogger(EntityBuilder.class);
 
   public static final String             GROUP = "group";
-
-  private static LayoutService           layoutService;
-
-  private static OrganizationService     organizationService;
-
-  private static UserACL                 userACL;
-
-  private static UserPortalConfigService userPortalConfigService;
-
   private EntityBuilder() { // NOSONAR
   }
 
@@ -130,108 +101,5 @@ public class EntityBuilder {
       result.add(resultNode);
     }
     return result;
-  }
-
-
-  public static SiteRestEntity toSiteRestEntity(PortalConfig site, HttpServletRequest request, SiteFilter siteFilter) {
-    if (site == null) {
-      return null;
-    }
-    SiteType siteType = SiteType.valueOf(site.getType().toUpperCase());
-    String displayName = site.getLabel();
-    Identity userIdentity = ConversationState.getCurrent().getIdentity();
-    if (SiteType.GROUP.equals(siteType)) {
-      try {
-        Group siteGroup = getOrganizationService().getGroupHandler().findGroupById(site.getName());
-        if (siteGroup == null || !userIdentity.isMemberOf(siteGroup.getId())) {
-          return null;
-        } else if (org.apache.commons.lang3.StringUtils.isBlank(displayName)) {
-          displayName = siteGroup.getLabel();
-        }
-      } catch (Exception e) {
-        LOG.error("Error while retrieving group with name ", site.getName(), e);
-      }
-    }
-    List<Map<String, Object>> accessPermissions = computePermissions(site.getAccessPermissions());
-    Map<String, Object> editPermission = computePermission(site.getEditPermission());
-
-    UserNode rootNode = null;
-    if (siteFilter.isExpandNavigations()) {
-      String currentUser = userIdentity.getUserId();
-      try {
-        HttpUserPortalContext userPortalContext = new HttpUserPortalContext(request);
-        UserPortalConfig userPortalCfg = getUserPortalConfigService().getUserPortalConfig(site.getName(), currentUser, userPortalContext);
-        UserPortal userPortal = userPortalCfg.getUserPortal();
-        UserNavigation navigation = userPortal.getNavigation(new SiteKey(siteType.getName(), site.getName()));
-        rootNode = userPortal.getNode(navigation, Scope.ALL, UserNodeFilterConfig.builder().build(), null);
-      } catch (Exception e) {
-        LOG.error("Error while getting site {} navigations for user {}", site.getName(), currentUser, e);
-      }
-    }
-    return new SiteRestEntity(siteType,
-            site.getName(),
-            !StringUtils.isBlank(displayName) ? displayName : site.getName(),
-            site.getDescription(),
-            accessPermissions,
-            editPermission,
-            isDefaultSite(site.getName()) || site.isDisplayed(),
-            site.getDisplayOrder(),
-            isDefaultSite(site.getName()),
-            rootNode == null ? null : toUserNodeRestEntity(rootNode.getChildren(), true, getOrganizationService(), getLayoutService(), getUserACL()));
-
-  }
-
-  public static List<SiteRestEntity> toSiteRestEntities(List<PortalConfig> sites,
-                                                        HttpServletRequest request,
-                                                        SiteFilter siteFilter) {
-    return sites.stream().map(site -> toSiteRestEntity(site, request, siteFilter)).filter(Objects::nonNull).toList();
-  }
-
-  private static Map<String, Object> computePermission(String permission) {
-    Map<String, Object> accessPermission = new HashMap<>();
-    try {
-      accessPermission.put("membershipType", permission.split(":")[0]);
-      accessPermission.put(GROUP, getOrganizationService().getGroupHandler().findGroupById(permission.split(":")[1]));
-    } catch (Exception e) {
-      LOG.error("Error while computing user permission ", permission, e);
-    }
-    return accessPermission;
-  }
-
-  private static List<Map<String, Object>> computePermissions(String[] permissions) {
-    return Arrays.stream(permissions).map(EntityBuilder::computePermission).toList();
-  }
-
-  private static OrganizationService getOrganizationService() {
-    if (organizationService == null) {
-      organizationService = ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(OrganizationService.class);
-    }
-    return organizationService;
-  }
-
-  private static LayoutService getLayoutService() {
-    if (layoutService == null) {
-      layoutService = ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(LayoutService.class);
-    }
-    return layoutService;
-  }
-
-  private static UserACL getUserACL() {
-    if (userACL == null) {
-      userACL = ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(UserACL.class);
-    }
-    return userACL;
-  }
-
-  private static UserPortalConfigService getUserPortalConfigService() {
-    if (userPortalConfigService == null) {
-      userPortalConfigService =
-                              ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(UserPortalConfigService.class);
-    }
-    return userPortalConfigService;
-  }
-
-  public static boolean isDefaultSite(String siteName) {
-    return getUserPortalConfigService().getDefaultPortal().equals(siteName);
   }
 }
