bf6cae09948113f29a2fa50e0ca0fed995e91b77
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-05-17 15:03:53 +0100

    fix: Fix Forbidden on Spring REST response code - Meeds-io/meeds#2002 (#890)
    Prior to this change, when accessing a forbidden resource, a HTTP 200
    response code is returned. This change ensures to return the right
    response code HTTP 403.

---
+++ component/common/src/test/java/io/meeds/spring/module/rest/TestController.java
@@ -0,0 +1,47 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.spring.module.rest;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+  @GetMapping("administrators")
+  @Secured("administrators")
+  public void adminAccess() {
+    // Nothing to do, just testing integration with @Secured
+  }
+
+  @GetMapping("users")
+  @Secured("users")
+  public void userAccess() {
+    // Nothing to do, just testing integration with @Secured
+  }
+
+  @GetMapping("anonymous")
+  public void anonymousAccess() {
+    // Nothing to do, just testing integration with @Secured
+  }
+
+}
