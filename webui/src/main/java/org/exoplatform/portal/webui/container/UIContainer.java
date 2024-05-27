93d31e524c7551d8862f6d5bc27a720b46d740d9
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-05-27 17:51:32 +0100

    feat: Cleanup WebUI and Move App Registry to layout addon - MEED-6903 - Meeds-io/MIPs#139 (#898)
    Prior to this change, the App registry was handling portlets and
    categories. This change will move the App Registry in layout addon with
    Portlet instance instead of Portlet Definition itself. In addition, this
    change will merge 5 artifacts into one single knowing that WebUI has
    become a Portal Management framework only.

---
+++ webui/src/main/java/org/exoplatform/portal/webui/container/UIContainer.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright (C) 2009 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.exoplatform.portal.webui.container;
+
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.portal.config.UserACL;
+import org.exoplatform.portal.webui.portal.UIPortalComponent;
+import org.exoplatform.webui.config.annotation.ComponentConfig;
+
+/**
+ * May 19, 2006
+ */
+@ComponentConfig
+public class UIContainer extends UIPortalComponent {
+  public static final String TABLE_COLUMN_CONTAINER = "TableColumnContainer";
+
+  /** Storage id. */
+  private String             storageId;
+
+  protected String           icon;
+
+  protected String           description;
+
+  protected String           cssClass;
+
+  protected String           profiles;
+
+  protected String[]         moveContainersPermissions;
+
+  protected String[]         moveAppsPermissions;
+
+  public String getStorageId() {
+    return storageId;
+  }
+
+  public void setStorageId(String storageId) {
+    this.storageId = storageId;
+  }
+
+  public String getIcon() {
+    return icon;
+  }
+
+  public void setIcon(String s) {
+    icon = s;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String desc) {
+    this.description = desc;
+  }
+
+  public String getCssClass() {
+    return cssClass;
+  }
+
+  public void setCssClass(String cssClass) {
+    this.cssClass = cssClass;
+  }
+
+  public String getProfiles() {
+    return profiles;
+  }
+
+  public void setProfiles(String profiles) {
+    this.profiles = profiles;
+  }
+
+  public String[] getMoveContainersPermissions() {
+    return moveContainersPermissions;
+  }
+
+  public void setMoveContainersPermissions(String[] moveContainersPermissions) {
+    this.moveContainersPermissions = moveContainersPermissions;
+  }
+
+  public boolean hasMoveContainersPermission() {
+    ExoContainer exoContainer = ExoContainerContext.getCurrentContainer();
+    UserACL acl = exoContainer.getComponentInstanceOfType(UserACL.class);
+    return acl.hasPermission(moveContainersPermissions);
+  }
+
+  public String[] getMoveAppsPermissions() {
+    return moveAppsPermissions;
+  }
+
+  public void setMoveAppsPermissions(String[] moveAppsPermissions) {
+    this.moveAppsPermissions = moveAppsPermissions;
+  }
+
+  public String getPermissionClasses() {
+    return "";
+  }
+
+}
---
+++ webui/src/main/java/org/exoplatform/portal/webui/container/UISimpleColumnContainer.java
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ * 
+ * Copyright (C) 2023 Meeds Association contact@meeds.io
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.exoplatform.portal.webui.container;
+
+import org.exoplatform.webui.config.annotation.ComponentConfig;
+
+@ComponentConfig(template = "system:/groovy/portal/webui/container/UISimpleColumnContainer.gtmpl")
+public class UISimpleColumnContainer extends UIContainer {
+
+  public static final String SIMPLE_COLUMN_CONTAINER = "SimpleColumnContainer";
+
+}
