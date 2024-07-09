618f216fff86758d0c0f4631a1c0e15c62bdd1a8
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-07-09 19:20:10 +0100

    feat: Allow to serve CSS resources by File content Hash and from App Context - MEED-7237 - Meeds-io/MIPs#134
    This change will allow to fine selecting modified resources to update when its content changes. This will enhance browser stored cache size when using PWA. Besides, this will allow for each application to serve its own CSS as well and allow to handle the CSS updates in each addon apart.

--- webui/src/test/java/org/exoplatform/portal/webui/workspace/UISharedLayoutTest.java
+++ webui/src/test/java/org/exoplatform/portal/webui/workspace/UISharedLayoutTest.java
@@ -71,7 +71,7 @@ public class UISharedLayoutTest {
       }
 
       @Override
-      protected boolean isShowSharedLayout(PortalRequestContext requestContext) {
+      public boolean isShowSharedLayout(PortalRequestContext requestContext) {
         return !pcontext.isHideSharedLayout();
       }
     };
