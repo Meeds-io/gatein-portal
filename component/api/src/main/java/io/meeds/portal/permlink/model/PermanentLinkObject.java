959fb78ba059506d607dc750e6cc379d83998a0f
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-07-21 15:47:58 +0100

    feat: Implement Permanent Link API - Meeds-io/MIPs#134

--- component/api/src/main/java/io/meeds/portal/permlink/model/PermanentLinkObject.java
+++ component/api/src/main/java/io/meeds/portal/permlink/model/PermanentLinkObject.java
@@ -1,39 +1,51 @@
-/*
+/**
  * This file is part of the Meeds project (https://meeds.io/).
- * 
+ *
  * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-package org.exoplatform.services.thumbnail;
+package io.meeds.portal.permlink.model;
 
-@FunctionalInterface
-public interface ImageResizeService {
+import java.util.HashMap;
+import java.util.Map;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    /**
-     * Create a scaled image by resizing an initial given image
-     *
-     * @param image Target image content to be resized
-     * @param width Target resized image width
-     * @param height Target resized image height
-     * @param fitExact Fit resized image to the exact given with and height (true) or automatic fit (false)
-     * @param ultraQuality return an ultra quality resized image (may take more execution time)
-     * @return byte array of image content
-     * @throws Exception
-     */
-    byte[] scaleImage(byte[] image, int width, int height, boolean fitExact, boolean ultraQuality) throws Exception;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PermanentLinkObject {
+
+  private String              objectType;
+
+  private String              objectId;
+
+  private Map<String, String> parameters;
+
+  public PermanentLinkObject(String objectType, String objectId) {
+    this.objectType = objectType;
+    this.objectId = objectId;
+  }
+
+  public void addParameter(String key, String value) {
+    if (parameters == null) {
+      parameters = new HashMap<>();
+    }
+    parameters.put(key, value);
+  }
 
 }
