935bb3a1404c54ed6d5cd45428ba79fcfe30abdf
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-10-21 13:11:45 +0100

    feat: Allow Global Pages In Space Context and cleanup useless Classes - Meeds-io/MIPs#150 (#989)

--- component/portal/src/main/java/io/meeds/portal/security/listener/DefaultUserMembershipListener.java
+++ component/portal/src/main/java/io/meeds/portal/security/listener/DefaultUserMembershipListener.java
@@ -21,9 +21,9 @@ package io.meeds.portal.security.listener;
 import java.util.Calendar;
 import java.util.Collection;
 
-import org.apache.commons.codec.binary.StringUtils;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import org.exoplatform.container.ExoContainer;
 import org.exoplatform.services.listener.Event;
