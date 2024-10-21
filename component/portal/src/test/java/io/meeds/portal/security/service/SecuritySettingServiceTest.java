1e879951393ecedd5fb54196701c20f47411dbae
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-10-21 13:11:45 +0100

    feat: Allow Global Pages In Space Context and cleanup useless Classes - Meeds-io/MIPs#150 (#989)

--- component/portal/src/test/java/io/meeds/portal/security/service/SecuritySettingServiceTest.java
+++ component/portal/src/test/java/io/meeds/portal/security/service/SecuritySettingServiceTest.java
@@ -42,7 +42,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.apache.commons.codec.binary.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
